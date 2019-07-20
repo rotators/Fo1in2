628b2508a604a0188d7a127826985d1aa11f5bc0
MrLexx <MrLexx@47b89411-515a-42cf-9b56-1c54e348859e>
2019-07-20 16:51:40 +0000

    - improvements to the encounter system. - manually going into an encounter now puts the player in either city, desert, coast or mountain map.

--- trunk/Fallout2/Fallout1Port/Mapper/source/scripts/HEADERS/scripts.h
+++ trunk/Fallout2/Fallout1Port/Mapper/source/scripts/HEADERS/scripts.h
@@ -993,5 +993,6 @@
 //Lexx
 #define SCRIPT_CHEATBOX  (981)
 #define SCRIPT_NIF2HINT  (982)  // niF2Hint.int    ; New Reno Fallout 2 Hintbook
+#define SCRIPT_RNDENC 	 (983)	// Random encounter map script
 
 #endif // SCRIPTS_H
