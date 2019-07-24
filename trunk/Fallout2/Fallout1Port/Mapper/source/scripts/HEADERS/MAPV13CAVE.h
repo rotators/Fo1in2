8a8fb2d3112b3af3c9c216de63f83c0763e84c40
MrLexx <MrLexx@47b89411-515a-42cf-9b56-1c54e348859e>
2019-07-24 04:23:43 +0000

    - forgot the (yet empty) header.

---
+++ trunk/Fallout2/Fallout1Port/Mapper/source/scripts/HEADERS/MAPV13CAVE.h
@@ -0,0 +1,24 @@
+/*
+
+	Necropolis header
+	
+*/
+
+#ifndef MAPV13CAVE_H
+#define MAPV13CAVE_H
+
+/************************************************
+    Generic Defines
+************************************************/
+
+//MAP_GLOBAL_VARS:
+
+//GLOBAL                                        NUMBER
+// timed event params
+
+//==================================================================
+
+// All Map Vars need to start w/ MVAR_
+
+
+#endif // MAPV13CAVE_H
