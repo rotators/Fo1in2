4a502cf48d7e50661f240b85adcf273b71e636e4
wipe <wipe@47b89411-515a-42cf-9b56-1c54e348859e>
2019-07-27 06:23:26 +0000

    ported reading [Defines] and headers

--- trunk/Tools/ReDefine/Source/Defines.cpp
+++ trunk/Tools/ReDefine/Source/Defines.cpp
@@ -1,13 +1,52 @@
 #include "FOClassic/Ini.h"
 
 #include "ReDefine.h"
-#include "Defines.h"
 
-bool ReDefine::ReadDefines( const std::string& path, const std::string& filename, const std::string& type, const std::string& prefix, const std::string& group )
+// void ReDefine::InitDefines() {}
+
+void ReDefine::FinishDefines()
+{
+    RegularDefines.clear();
+    VirtualDefines.clear();
+}
+
+bool ReDefine::ReadConfigDefines()
+{
+    if( !Config->IsSection( "Defines" ) )
+    {
+        WARNING( __FUNCTION__, "config section<Defines> is missing" );
+        return false;
+    }
+
+    std::vector<std::string> keys;
+
+    if( !Config->GetSectionKeys( "Defines", keys, true ) )
+    {
+        WARNING( __FUNCTION__, "config section<Defines> is empty" );
+        return false;
+    }
+
+    std::vector<std::string> values;
+    for( auto& type : keys )
+    {
+        values = Config->GetStrVec( "Defines", type );
+
+        if( values.size() == 2 )
+            ReadDefines( values[0], type, values[1] );
+        else if( values.size() == 3 )
+            ReadDefines( values[0], type, values[1], values[2] );
+        else
+            WARNING( __FUNCTION__, "invalid setting<[Defines]->%s> :: %s", type.c_str(), Config->GetStr( "Defines", type ).c_str() );
+    }
+
+    return true;
+}
+
+bool ReDefine::ReadDefines( const std::string& filename, const std::string& type, const std::string& prefix, const std::string& group )
 {
     if( type.empty() )
     {
-        WARNING( __FUNCTION__, "missing type name%s", "" );
+        WARNING( __FUNCTION__, "missing type name" );
         return false;
     }
     else if( filename.empty() )
@@ -21,31 +60,57 @@ bool ReDefine::ReadDefines( const std::string& path, const std::string& filename
         return false;
     }
 
-    if( Defines.find( type ) != Defines.end() || VirtualDefines.find( type ) != VirtualDefines.end() )
+    if( RegularDefines.find( type ) != RegularDefines.end() || VirtualDefines.find( type ) != VirtualDefines.end() )
     {
-        WARNING( __FUNCTION__, "type<%s> already in use", type.c_str() );
+        WARNING( __FUNCTION__, "define type<%s> already in use", type.c_str() );
         return false;
     }
 
-    Ini header;
-    header.KeepComments = true;
-    header.KeepSectionsRaw = true;
+    // read content
+    std::vector<std::string> lines;
+    if( !ReadFile( filename, lines ) )
+        return false;
 
-    header.LoadFile( filename );
+    // update status
+    Status.Current.File = filename;
+    Status.Current.LineNumber = 0;
 
-    for( auto& line : header.GetSectionRaw( "" ) )
+    // cache pattern
+    std::regex  paren = TextGetDefinePattern( prefix, true );
+    std::regex  noParen = TextGetDefinePattern( prefix, false );
+
+    std::string name;
+    int         value;
+
+    for( const std::string& line : lines )
     {
-        if( line.empty() )
+        Status.Current.LineNumber++;
+
+        // ignore random lines
+        if( !TextIsDefine( line ) )
             continue;
 
-        DEBUG( __FUNCTION__, line.c_str() );
+        // find defines with given prefix
+        if( TextGetDefine( line, paren, name, value ) || TextGetDefine( line, noParen, name, value ) )
+        {
+            // human detection
+            if( RegularDefines[type].find( value ) != RegularDefines[type].end() )
+            {
+                WARNING( nullptr, "value<%d> already used by define<%s>, current define<%s> ignored", value, RegularDefines[type][value].c_str(), name.c_str() );
+                continue;
+            }
+
+            RegularDefines[type][value] = name;
+
+            if( !group.empty() )
+                VirtualDefines[group].push_back( type );
+        }
+
     }
 
-    return true;
-}
+    Status.Current.Clear();
 
-void ReDefine::FinishDefines()
-{
-    Defines.clear();
-    VirtualDefines.clear();
+    LOG( "Reading %s... %u %s define%s%s%s", filename.c_str(), RegularDefines[type].size(), prefix.c_str(), RegularDefines[type].size() != 1 ? "s" : "", !group.empty() ? " -> " : "", group.c_str() );
+
+    return true;
 }
--- trunk/Tools/ReDefine/Source/ReDefine.h
+++ trunk/Tools/ReDefine/Source/ReDefine.h
@@ -2,39 +2,79 @@
 #define __REDEFINE__    // :)
 
 #include <map>
+#include <regex>
 #include <string>
 #include <vector>
 
 #include "Types.h"
 
 class CmdLine;
-class Define;
 class Ini;
 
-namespace ReDefine
+class ReDefine
 {
+public:
+
+    //
+    // ReDefine
+    //
+
+    CmdLine* CommandLine;
+    Ini*     Config;
+
     struct SStatus
     {
-        std::string CurrentFile;
-        std::string CurrentLine;
-        uint        CurrentLineNumber;
+        struct SCurrent
+        {
+            std::string File;
+            std::string Line;
+            uint        LineNumber;
 
-        void        Clear();
-    };
+            SCurrent();
+            void        Clear();
+        }
+        Current;
 
-    extern CmdLine* CommandLine;
-    extern Ini*     Config;
+        void Clear();
+    }
+    Status;
 
-    extern SStatus  Status;
+
+    ReDefine();
+    virtual ~ReDefine();
+
+    bool Init( int argc = 0, char** argv = nullptr );
+    void Finish();
 
     void DEBUG( const char* func, const char* format, ... );
     void WARNING( const char* func, const char* format, ... );
     void LOG( const char* format, ... );
 
     bool ReadFile( const std::string& path, std::vector<std::string>& lines );
+    bool ReadConfig( const std::string& config );
 
-    bool Init( int argc, char** argv, const char* config );
-    int  Exit( int returnValue );
+    //
+    // Defines
+    //
+
+    std::map<std::string, std::map<int, std::string>> RegularDefines;
+    std::map<std::string, std::vector<std::string>>   VirtualDefines;
+
+
+    bool ReadConfigDefines();
+    bool ReadDefines( const std::string& filename, const std::string& type, const std::string& prefix, const std::string& group = std::string() );
+    void FinishDefines();
+
+    //
+    // Text
+    //
+
+    bool TextIsComment( const std::string& text );
+    bool TextGetInt( const std::string& text, int& result, const unsigned char& base = 10 );
+
+    bool       TextIsDefine( const std::string& text );
+    bool       TextGetDefine( const std::string& text, const std::regex& pattern, std::string& name, int& value );
+    std::regex TextGetDefinePattern( const std::string& prefix, bool paren );
 };
 
 #endif // __REDEFINE__ //
