c5403f112f353236c1a974bf27548a288d09a387
Lexx2k <lexxnet@web.de>
2019-07-29 15:57:33 +0000

    - Fixed Rhombus not reacting if the player fiddles on his door.

---
+++ Fallout2/Fallout1Port/Mapper/source/scripts/HEADERS/MAPBROTHERHOOD.h
@@ -0,0 +1,47 @@
+/*
+
+	Brotherhood of Steel
+
+*/
+
+#ifndef MAPBROTHERHOOD_H
+#define MAPBROTHERHOOD_H
+
+/************************************************
+    Generic Defines
+************************************************/
+
+//MAP_GLOBAL_VARS:
+
+//GLOBAL                                        NUMBER
+// timed event params
+
+//==================================================================
+
+// All Map Vars need to start w/ MVAR_
+
+// BROHD12:
+#define MVAR_STUDENT_READY       (0)  
+#define MVAR_STUDENT_HEX         (1)  
+#define MVAR_SIGNAL_NEW_STUDENT  (2)  
+#define MVAR_SIGNAL_OLD_STUDENT  (3)  
+#define MVAR_I_AM_ACTIVE         (4)  
+#define MVAR_STUDENT_NUM         (5)  
+#define MVAR_SOPHIA_FLAG1        (6)  
+#define MVAR_SOPHIA_FLAG2        (7)  
+#define MVAR_SOPHIA_FLAG3        (8)  
+#define MVAR_ANY_REQ             (9)  
+#define MVAR_AMMO                (10)  
+#define MVAR_AMMO_TAKEN          (11)  
+#define MVAR_UZI                 (12)  
+#define MVAR_SIG_SAUER           (13)  
+#define MVAR_CBT_ARMOR           (14)  
+#define MVAR_PWR_ARMOR           (15)  
+#define MVAR_GIEGER              (16)  
+#define MVAR_AMMO_THIS_WEEK      (17)  
+#define MVAR_Door_Warning        (18)  
+#define MVAR_TOUCHING            (19)  
+#define MVAR_SUPER_SLEDGE        (20)  
+
+#endif // MAPBROTHERHOOD_H
+
