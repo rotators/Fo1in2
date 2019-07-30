130a8c5b13f38020e5fc0c8775b540b788ea1522
MrLexx <MrLexx@47b89411-515a-42cf-9b56-1c54e348859e>
2019-07-30 16:52:05 +0000

    - Mass compile of all scripts. - Fixed: Huge performance issue in Gun Runners location after player gets all weapons. - Fixed: (Maybe) Blades will correctly invade Adytum after the player gave the holodisk to Jon Zimmermann. - Fixed: After giving the water chip to the overseer, the player is correctly teleported to the library and files a report. - Fixed: Necropolis Vault glowing ghouls were invincible. - Fixed: Necropolis Watershed prisoner now walks correctly out of the map. Also replaced a useless gvar with an lvar. - Changed: Gizmo's guard Izo will behave like in thy olde days. - Also updated ai.txt file - needs testing.

---
+++ trunk/Fallout2/Fallout1Port/Mapper/source/scripts/HEADERS/MAPLAADYTUM.h
@@ -0,0 +1,32 @@
+/*
+
+	Boneyard, Adytum / Adytown
+
+*/
+
+#ifndef MAPLAADYTUM_H
+#define MAPLAADYTUM_H
+
+/************************************************
+    Generic Defines
+************************************************/
+
+//MAP_GLOBAL_VARS:
+
+//GLOBAL                                        NUMBER
+// timed event params
+
+//==================================================================
+
+// All Map Vars need to start w/ MVAR_
+#define MVAR_visited_before         (0) // Set to TRUE if the player has been here.
+#define MVAR_RegulatorCount         (1) // Number of Regulators on the map.
+#define MVAR_AddedBlades            (2) // Have the blades been added to the map.
+#define MVAR_InvasionForcesCreated  (3) // Have the invasion forces been created?
+
+
+#endif // MAPLAADYTUM_H
+
+
+
+
