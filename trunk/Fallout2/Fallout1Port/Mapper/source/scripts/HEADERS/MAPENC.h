8a6b9fdfb32430ad3171c59bdcc4d7f5ad13ef68
MrLexx <MrLexx@47b89411-515a-42cf-9b56-1c54e348859e>
2019-07-31 20:22:27 +0000

    - cleanup

--- trunk/Fallout2/Fallout1Port/Mapper/source/scripts/HEADERS/MAPENC.h
+++ trunk/Fallout2/Fallout1Port/Mapper/source/scripts/HEADERS/MAPENC.h
@@ -11,23 +11,23 @@
     Generic Defines
 ************************************************/
 
-#define enc_group_angle(x) 			if (group_angle == 0) then begin \
-										x := 3;\
-									end\
-									else if (group_angle == 1) then begin\
-										x := 4;\
-									end\
-									else if (group_angle == 2) then begin\
-										x := 5;\
-									end\
-									else if (group_angle == 3) then begin\
-										x := 0;\
-									end\
-									else if (group_angle == 4) then begin\
-										x := 1;\
-									end\
-									else if (group_angle == 5) then begin\
-										x := 2;\
+#define enc_group_angle(x) 			if (group_angle == 0) then begin 		\
+										x := 3;								\
+									end										\
+									else if (group_angle == 1) then begin	\
+										x := 4;								\
+									end										\
+									else if (group_angle == 2) then begin	\
+										x := 5;								\
+									end										\
+									else if (group_angle == 3) then begin	\
+										x := 0;								\
+									end										\
+									else if (group_angle == 4) then begin	\
+										x := 1;								\
+									end										\
+									else if (group_angle == 5) then begin	\
+										x := 2;								\
 									end
 
 
