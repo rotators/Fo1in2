426ab4f90c7a1756148a203fdc615a9972961a1c
wipe <wipe@47b89411-515a-42cf-9b56-1c54e348859e>
2019-08-01 00:42:04 +0000

    ReDefine get_pc_stat(); testing new shiny c++ port

--- trunk/Fallout2/Fallout1Port/Mapper/source/scripts/HEADERS/gl_worldmap.h
+++ trunk/Fallout2/Fallout1Port/Mapper/source/scripts/HEADERS/gl_worldmap.h
@@ -7,9 +7,9 @@
 #define WM_TERRAIN_MOUNTAIN (3)
 #define WM_TERRAIN_OCEAN    (4)
 
-#define is_terrain_desert 				(global_var(WORLD_TERRAIN_TYPE) == WM_TERRAIN_DESERT)
-#define is_terrain_city 				(global_var(WORLD_TERRAIN_TYPE) == WM_TERRAIN_CITY)
-#define is_terrain_mountain 			(global_var(WORLD_TERRAIN_TYPE) == WM_TERRAIN_MOUNTAIN)
-#define is_terrain_ocean 				(global_var(WORLD_TERRAIN_TYPE) == WM_TERRAIN_OCEAN)
+#define is_terrain_desert   (global_var(WORLD_TERRAIN_TYPE) == WM_TERRAIN_DESERT)
+#define is_terrain_city     (global_var(WORLD_TERRAIN_TYPE) == WM_TERRAIN_CITY)
+#define is_terrain_mountain (global_var(WORLD_TERRAIN_TYPE) == WM_TERRAIN_MOUNTAIN)
+#define is_terrain_ocean    (global_var(WORLD_TERRAIN_TYPE) == WM_TERRAIN_OCEAN)
 
 #endif // GL_WORLDMAP_H //
