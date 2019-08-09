577515a2084cb02332410df4d1e4b650161300e6
Wipe <wipe.git@gmail.com>
2019-08-09 07:33:19 +0000

    *nix-friendly debug.h/define.h

--- Fallout2/Fallout1Port/Mapper/source/scripts/headers/debug.h
+++ Fallout2/Fallout1Port/Mapper/source/scripts/headers/debug.h
@@ -1,7 +1,7 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
-#include "GLOBAL.H"
+#include "global.h"
 
 #define debug_mode  (global_var(DEBUG_MODE_MESSAGES_ON) == 1)
 #define debug(x)    if debug_mode then display_msg(x)
