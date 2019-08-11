8482c1571dca876134ba0bdf6d92498194ba550e
MrLexx <MrLexx@47b89411-515a-42cf-9b56-1c54e348859e>
2019-08-11 07:26:41 +0000

    - Some cleanup. - Fixed: Systolic motivation question after talking to Michael once before.

---
+++ trunk/Fallout2/Fallout1Port/Mapper/source/scripts/headers/mapshady.h
@@ -0,0 +1,33 @@
+/*
+
+	Shady Sands
+
+*/
+
+#ifndef MAPSHADY_H
+#define MAPSHADY_H
+
+/************************************************
+    Generic Defines
+************************************************/
+
+//MAP_GLOBAL_VARS:
+
+//GLOBAL                                        NUMBER
+// timed event params
+
+//==================================================================
+
+// All Map Vars need to start w/ MVAR_
+// ShadyE:
+#define MVAR_Invasion             (0) // Has the town been invaded by Super Mutants?
+
+//ShadyW:
+#define MVAR_WARNED               (0) // has the player been warned to put his weapon away?
+#define MVAR_TANDI_EXP            (1)
+#define MVAR_Caught_Stealing      (2)
+#define MVAR_Map_Only_once        (3)
+#define MVAR_Map_one_Time         (4)
+#define MVAR_Invasion             (5)
+
+#endif // MAPSHADY_H
