16c830dba15eb5661b6127041be2e52d368184aa
MrLexx <MrLexx@47b89411-515a-42cf-9b56-1c54e348859e>
2019-08-12 18:22:42 +0000

    - Power Armor protecc

--- trunk/Fallout2/Fallout1Port/Mapper/source/scripts/headers/command.h
+++ trunk/Fallout2/Fallout1Port/Mapper/source/scripts/headers/command.h
@@ -237,6 +237,11 @@ variable step_tile;
                                              (obj_pid(critter_inven_obj(dude_obj,INVEN_TYPE_WORN)) == PID_ADVANCED_POWER_ARMOR) or \
                                              (obj_pid(critter_inven_obj(dude_obj,INVEN_TYPE_WORN)) == PID_ADVANCED_POWER_ARMOR_MK2) or \
                                              (obj_pid(critter_inven_obj(dude_obj,INVEN_TYPE_WORN)) == PID_HARDENED_POWER_ARMOR))
+											 
+#define critter_wearing_power_armor(x)      ((obj_pid(critter_inven_obj(x,INVEN_TYPE_WORN)) == PID_POWERED_ARMOR) or \
+                                             (obj_pid(critter_inven_obj(x,INVEN_TYPE_WORN)) == PID_ADVANCED_POWER_ARMOR) or \
+                                             (obj_pid(critter_inven_obj(x,INVEN_TYPE_WORN)) == PID_ADVANCED_POWER_ARMOR_MK2) or \
+                                             (obj_pid(critter_inven_obj(x,INVEN_TYPE_WORN)) == PID_HARDENED_POWER_ARMOR))
 
 #define dude_wearing_leather_armor          ((obj_pid(critter_inven_obj(dude_obj,INVEN_TYPE_WORN)) == PID_LEATHER_ARMOR) or \
                                              (obj_pid(critter_inven_obj(dude_obj,INVEN_TYPE_WORN)) == PID_LEATHER_JACKET) or \
