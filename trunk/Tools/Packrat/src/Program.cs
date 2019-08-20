98c009dcf1b19c43b15d1f6b31260d0f8356ee81
Ghosthack <Ghosthack@47b89411-515a-42cf-9b56-1c54e348859e>
2019-08-20 23:35:48 +0000

    I made something that sucks less than datexplorer.

---
+++ trunk/Tools/Packrat/src/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Packrat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var file = DatFile.FromFile(@"D:\Fallout\dev\Fo1Port\trunk\Fallout2\Fallout1Port\data\art\inven\CAP2.FRM", @"art\inven", true);
+            //var file2 = DatFile.FromFile(@"D:\Fallout\dev\Fo1Port\trunk\Fallout2\Fallout1Port\data\art\inven\zipgun.FRM", @"art\inven", true);
+            //Dat.CreateDat(Directory.GetCurrentDirectory() + "\\test.dat", new List<DatFile> { file, file2});
+            Script.Run("script.pack");
+            Console.ReadKey();
+        }
+    }
+}
