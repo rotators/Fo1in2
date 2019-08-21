012a327a27dabcca84e110736f373b3a6405aa9f
Lexx2k <lexxnet@web.de>
2019-08-21 16:40:01 +0000

    - define renamed

--- Fallout2/Fallout1Port/Mapper/source/scripts/headers/voodoo.h
+++ Fallout2/Fallout1Port/Mapper/source/scripts/headers/voodoo.h
@@ -4,16 +4,16 @@
 // memory editing macros
 //
 
-// ???
-#define VOODOO_no_idea_1 \
-               write_int( 0x4A4343, 0x90909090); \
+// This will disable running the credits after the endgame slides:
+#define VOODOO_mode_fo1_ending						\
+               write_int( 0x4A4343, 0x90909090); 	\
                write_byte(0x4A4347, 0x90)
 
 // This will disable the "You encounter: ..." message:
-#define VOODOO_disable_YouEncounter_message \
-               write_int( 0x4C1011,  0x90909090); \
-               write_byte(0x4C1015, 0x90); \
-               write_int( 0x4C1042,  0x90909090); \
+#define VOODOO_disable_YouEncounter_message 		\
+               write_int( 0x4C1011,  0x90909090); 	\
+               write_byte(0x4C1015, 0x90); 			\
+               write_int( 0x4C1042,  0x90909090); 	\
                write_byte(0x4C1046, 0x90)
 
 #endif // VOODOO_H //
