53652bc0f92148d6f701c3b77133632d497cda4b
Wipe <wipe.git@gmail.com>
2019-08-22 11:29:08 +0000

    PackRat - script filename can be set from command line - alternative script filename: script.packrat - sources only, cannot into VS

---
+++ Tools/Packrat/csc.cmd
@@ -0,0 +1,7 @@
+@REM
+@REM For quick compilation without VS (%WINDIR%\Microsoft.Net\Framework\v*\csc.exe won't work [for average user], as it supports C#5 only)
+@REM
+@REM https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+@REM nuget.exe install Microsoft.Net.Compilers -ExcludeVersion
+@REM
+Microsoft.Net.Compilers\tools\csc.exe src\Program.cs src\dat.cs src\script.cs
