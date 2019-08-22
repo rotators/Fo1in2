b6044c8aa74e164b0bf78ee2197a571f7db42ee5
wipe <wipe@47b89411-515a-42cf-9b56-1c54e348859e>
2019-08-22 11:29:08 +0000

    PackRat - script filename can be set from command line - alternative script filename: script.packrat - sources only, cannot into VS

--- trunk/Tools/Packrat/src/Program.cs
+++ trunk/Tools/Packrat/src/Program.cs
@@ -13,7 +13,28 @@ namespace Packrat
         {
             System.Threading.Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.CreateSpecificCulture("en-US");
             System.Threading.Thread.CurrentThread.CurrentUICulture = System.Threading.Thread.CurrentThread.CurrentCulture;
-            Script.Run("script.pack");
+
+            if( args.Length > 0 && args[0].Length > 0 )
+            {
+              if( File.Exists( args[0] ))
+                Script.Run( args[0] );
+              else
+              {
+                Console.WriteLine( $"Cannot find packing script ({args[0]})" );
+                return;
+              }
+            }
+            else if( File.Exists( "script.pack" ))
+              Script.Run("script.pack");
+            else if( File.Exists( "script.packrat" ))
+              Script.Run("script.packrat");
+            else
+            {
+              Console.WriteLine( "Cannot find packing script (script.pack or script.packrat) and no filename provided" );
+              return;
+            }
+
+            // should be ReadKey() ... which throws System.InvalidOperationException in git-bash :/
             Console.Read();
         }
     }
