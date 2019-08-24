f29f712b378165fbce29b660b3095ae6a0a6323c
Lexx2k <lexxnet@web.de>
2019-08-24 23:32:04 +0000

    - Fixed: Flip in Vats map won't bring the player to Lou if he attacks with the Brotherhood.

---
+++ Fallout2/Fallout1Port/Mapper/source/scripts/headers/mbase.h
@@ -0,0 +1,17 @@
+/*
+
+	Military Base generic defines
+
+*/
+
+#ifndef MBASE_H
+#define MBASE_H
+
+/************************************************
+    Generic Defines
+************************************************/
+
+#define bos_invading_cathedral   (global_var( GVAR_BROTHERHOOD_SEND_HELP ) == 1)
+#define bos_invading_mbase       (global_var( GVAR_BROTHERHOOD_SEND_HELP ) == 2)
+
+#endif // MBASE_H
