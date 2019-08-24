713ef4a53f369355fa7235eacf2ad12a8b613027
Lexx2k <lexxnet@web.de>
2019-08-24 21:30:31 +0000

    - Changed lvar to mvar, because there are multiple terminals in the same map and we want to give the EXP only once.

---
+++ Fallout2/Fallout1Port/Mapper/source/scripts/headers/mapmstrlr12.h
@@ -0,0 +1,35 @@
+/*
+
+	Master's Vault
+
+*/
+
+#ifndef MAPMSTRLR12_H
+#define MAPMSTRLR12_H
+
+/************************************************
+    Generic Defines
+************************************************/
+
+//MAP_GLOBAL_VARS:
+
+//GLOBAL                                        NUMBER
+// timed event params
+
+//==================================================================
+
+// All Map Vars need to start w/ MVAR_
+#define MVAR_EXTRA                (0)  // has the player been warned to put his weapon away?
+#define MVAR_JERNEAR              (1)
+#define MVAR_FIELDOFF             (2)
+#define MVAR_SECRET_DOOR          (3)  // spatial trigger to open secret door
+#define MVAR_PLACEHOLDER          (4)
+#define MVAR_TOUCHING             (5)
+#define MVAR_Psy_Field_Status     (6)
+#define MVAR_Pris_Field_Status    (7)
+#define MVAR_PLACEHOLDER1         (8)
+#define MVAR_PLACEHOLDER2         (9)
+#define MVAR_INVADED              (10)  // have i placed critters here
+#define MVAR_TERMINAL_EXP 	      (11)  // player got the terminal experience bonus already?
+
+#endif // MAPMSTRLR12_H
