c42965d1bedbfa51c4166c33262b6dd89b46eff5
Markus <ghsthack@gmail.com>
2019-08-29 00:52:37 +0000

    Tool to read FO2 memory and display structures.

---
+++ Tools/Overseer/frmMain.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Overseer
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnAttach = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.grOffset = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.grName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.grType = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.grValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tmrRefresh = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.grOffset,
+            this.grName,
+            this.grType,
+            this.grValue});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 49);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 339);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // btnAttach
+            // 
+            this.btnAttach.Location = new System.Drawing.Point(12, 13);
+            this.btnAttach.Name = "btnAttach";
+            this.btnAttach.Size = new System.Drawing.Size(121, 30);
+            this.btnAttach.TabIndex = 2;
+            this.btnAttach.Text = "Attach";
+            this.btnAttach.UseVisualStyleBackColor = true;
+            this.btnAttach.Click += new System.EventHandler(this.BtnAttach_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(214, 13);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(106, 30);
+            this.btnRefresh.TabIndex = 3;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.BtnRefresh_Click);
+            // 
+            // grOffset
+            // 
+            this.grOffset.HeaderText = "Offset";
+            this.grOffset.Name = "grOffset";
+            // 
+            // grName
+            // 
+            this.grName.HeaderText = "Name";
+            this.grName.Name = "grName";
+            // 
+            // grType
+            // 
+            this.grType.HeaderText = "Type";
+            this.grType.Name = "grType";
+            // 
+            // grValue
+            // 
+            this.grValue.HeaderText = "Value";
+            this.grValue.Name = "grValue";
+            // 
+            // tmrRefresh
+            // 
+            this.tmrRefresh.Tick += new System.EventHandler(this.TmrRefresh_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 405);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnAttach);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "frmMain";
+            this.Text = "Overseer";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnAttach;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn grOffset;
+        private System.Windows.Forms.DataGridViewTextBoxColumn grName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn grType;
+        private System.Windows.Forms.DataGridViewTextBoxColumn grValue;
+        private System.Windows.Forms.Timer tmrRefresh;
+    }
+}
+
---
+++ Tools/Overseer/frmMain.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Overseer
+{
+    public partial class frmMain : Form
+    {
+        Memory mem;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAttach_Click(object sender, EventArgs e)
+        {
+            var fallout2 = Process.GetProcessesByName("Fallout2");
+            if (fallout2.Length > 0)
+            {
+                mem = new Memory(fallout2[0].Id);
+                tmrRefresh.Enabled = true;
+            }
+        }
+
+        private void AddRow(int offset, string name, string datatype, string value)
+        {
+            this.dataGridView1.Rows.Add(new string[] { "0x" + offset.ToString("x"), name, datatype, value});
+        }
+
+        private void AddByte(int offset, string name)
+        {
+            dataGridView1.Rows.Add(offset, name, "Byte", mem.ReadByte(offset).ToString());
+        }
+
+        private void AddInt16(int offset, string name)
+        {
+            this.AddRow(offset, name, "Int16", mem.ReadInt16(offset).ToString());
+        }
+
+        private void AddString(int offset, string name)
+        {
+            this.AddRow(offset, name, "String", mem.ReadString(offset, out int _).ToString());
+        }
+
+        private void BtnRefresh_Click(object sender, EventArgs e)
+        {
+            this.ReadMemory();
+        }
+
+        private List<PathNode> ReadPaths()
+        {
+            var paths = new List<PathNode>();
+            var deref = mem.ReadInt(0x6B24D0);
+            var pn = mem.ReadPathNode(deref);
+            paths.Add(pn);
+            while(pn.nextPtr != 0)
+            {
+                var off = mem.ReadInt(pn.nextPtr);
+                pn = mem.ReadPathNode(pn.nextPtr);
+                paths.Add(pn);
+            }
+            return paths;
+        }
+
+        private List<ScriptListInfo> ReadScriptList()
+        {
+            var s = new List<ScriptListInfo>();
+            var offset = mem.ReadInt(0x51C7C8); // dereference pointer
+            var scriptInfo = mem.ReadScriptListInfo(offset);
+            while (scriptInfo != null)
+            {
+                s.Add(scriptInfo);
+                offset += 20;
+                scriptInfo = mem.ReadScriptListInfo(offset);
+            }
+            return s;
+        }
+
+        private void ReadMemory()
+        {
+            this.dataGridView1.Rows.Clear();
+            AddByte(0x672E0C, "worldPosX");
+            AddByte(0x672E10, "worldPosY");
+            AddByte(0x6681B0, "playerLevel");
+            AddInt16(0x6AC7A8, "mouseX");
+            AddInt16(0x6AC7A4, "mouseY");
+            AddString(0x56D75C, "playerName");
+            //ReadPaths();
+            var scripts = ReadScriptList();
+        }
+
+        private void TmrRefresh_Tick(object sender, EventArgs e)
+        {
+            this.ReadMemory();
+        }
+    }
+}
