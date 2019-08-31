a4189a803427d64b45a9718275abb2fa92ef86e2
Lexx2k <lexxnet@web.de>
2019-08-31 20:20:01 +0000

    - Fixed: ERRORs in Mr. Handy companion dialog. - Fixed: Lou / Glow holodisk mixup. - Fixed: "Gain access to the Glow"-quest now appears correctly when entering the location. - Fixed: "Restore power"-quest in the Glow now starting and ticking off correctly. - Added: Scrapheap / Fo1 demo map and location data (not available ingame yet).

--- Fallout2/Fallout1Port/Mapper/source/scripts/headers/glow.h
+++ Fallout2/Fallout1Port/Mapper/source/scripts/headers/glow.h
@@ -14,15 +14,16 @@
 #define glow_power_emergency        (global_var(GVAR_GLOW_POWER) == 1)
 #define glow_power_online           (global_var(GVAR_GLOW_POWER) == 2)
 
-#define glow_quest_power_restored      (global_var(GVAR_QUEST_GLOW_2_START_POWER) == 2)
-#define set_glow_quest_power_restored  set_global_var(GVAR_QUEST_GLOW_2_START_POWER, 2)
-
 #define set_glow_power_offline      set_global_var(GVAR_GLOW_POWER, 0)
 #define set_glow_power_emergency    set_global_var(GVAR_GLOW_POWER, 1)
 #define set_glow_power_online       set_global_var(GVAR_GLOW_POWER, 2)
 
+#define glow_quest_power_restored      (global_var(GVAR_QUEST_GLOW_2_START_POWER) == 2)
+#define set_glow_quest_power_restored  set_global_var(GVAR_QUEST_GLOW_2_START_POWER, 2)
+
 #define glow_traps_active           (global_var(GVAR_QUEST_GLOW_1_TRAPS_OFF) == 0)
 #define glow_traps_inactive         (global_var(GVAR_QUEST_GLOW_1_TRAPS_OFF) == 1)
+
 #define set_glow_taps_active        set_global_var(GVAR_QUEST_GLOW_1_TRAPS_OFF, 0)
 #define set_glow_taps_inactive      set_global_var(GVAR_QUEST_GLOW_1_TRAPS_OFF, 1)
 
