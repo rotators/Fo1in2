15b15271626872cb405248e4bb523d7e2bebe339
MrLexx <MrLexx@47b89411-515a-42cf-9b56-1c54e348859e>
2019-09-01 11:48:13 +0000

    - Added: New quest defines for Shady Sands. Also some script cleanup. - Added: Proper quest log entry triggers.

---
+++ trunk/Fallout2/Fallout1Port/Mapper/source/scripts/headers/maps/shadyw.h
@@ -0,0 +1,24 @@
+/*
+
+	Shady Sands west side map (Townhall)
+
+*/
+
+#ifndef SHADYW_H
+#define SHADYW_H
+
+/************************************************
+    Generic Defines
+************************************************/
+
+// All Map Vars need to start w/ MVAR_
+#define MVAR_WARNED               (0) // has the player been warned to put his weapon away?
+#define MVAR_TANDI_EXP            (1)
+#define MVAR_Caught_Stealing      (2)
+#define MVAR_Map_Only_once        (3)
+#define MVAR_Map_one_Time         (4)
+#define MVAR_Invasion             (5)
+
+#endif // SHADYW_H
+
+
