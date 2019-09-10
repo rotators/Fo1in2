426519757cbe2fe1577e9d9d03f9e6f6bebc2ad3
wipe <wipe@47b89411-515a-42cf-9b56-1c54e348859e>
2019-09-10 18:12:57 +0000

    ReDefine update fc8779baf6f0ffbc4076331355d1a77a53af0dd5

--- trunk/Tools/ReDefine/ReDefine.nightly.sh
+++ trunk/Tools/ReDefine/ReDefine.nightly.sh
@@ -6,7 +6,7 @@ zip="/tmp/ReDefine.zip"
 exe="ReDefine.exe"
 
 curl -L "https://ci.appveyor.com/api/projects/rotators/ReDefine/artifacts/ReDefine.zip?job=Image:%20Visual%20Studio%202017;%20Platform:%20x32&branch=master&pr=false" -o $zip
-7z x -aoa $zip
+7z x -aoa $zip $exe
 chmod a+x $exe
 rm -f $zip
 file $exe
