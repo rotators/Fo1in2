390991b25c13836da344c79ad52ccd20a881d473
Wipe <wipe.git@gmail.com>
2019-10-26 22:58:54 +0200

    gha don't print info about all-fine scripts

--- .github/workflows/compile.sh
+++ .github/workflows/compile.sh
@@ -1,59 +1,45 @@
 #!/bin/bash
 
 compile_exe="$(pwd)/Tools/sFall_ScriptEditor/resources/compile.exe"
-compile_log="$(pwd)/compile.log"
 scripts_dir="$(pwd)/Fallout2/Fallout1in2/Mapper/source/scripts"
-
-# local testing
-if [ -f $compile_log ]; then
-   rm $compile_log
-fi
-
-# ssl files with errors
-ssl_errors=()
-
-for ssl_full in $scripts_dir/**/*.[Ss][Ss][Ll]; do
-    ssl_file=$(basename $ssl_full)
-    ssl_dir=$(dirname $ssl_full)
-    ssl_show=$(echo "$ssl_full" | sed -e "s!^$scripts_dir/!!")
-    int_file=$(echo "$ssl_file" | sed -e 's!\.[Ss][Ss][Ll]$!\.int!')
-
-    echo ::group::Compiling $ssl_show
-
-    # ssl compiler is too dumb to understand paths and always checks current directory
-    cd $ssl_dir
-    $compile_exe -q -n -l -p -s -O2 $ssl_file -o $int_file | tee -a $compile_log
-
-    # log cleanup
-    sed -i '/^$/d' $compile_log
-    sed -i '/^Compiling /d' $compile_log
-    sed -i '/THERE WERE ERRORS/g' $compile_log
-
-    if [ -f "$int_file" ]; then
-       rm $int_file
-    else
-       ssl_errors+=("$ssl_show")
-    fi
-
-    echo ::endgroup::
-done
 
 exit_val=0
 
-# !ssl_errors.empty()
-if [ "${#ssl_errors[@]}" -gt 0 ]; then
-   exit_val=1
-   echo "ERRORS"
-   for error in ${ssl_errors[@]}; do
-       echo "  $error"
-   done
-   echo ""
-fi
+echo Compiling...
 
-# exist and not empty
-if [ -s "$compile_log" ]; then
-   cat $compile_log
-fi
+for ssl_full in $scripts_dir/**/*.[Ss][Ss][Ll]; do
+    local ssl_file=$(basename $ssl_full)
+    local ssl_dir=$(dirname $ssl_full)
+    local ssl_show=$(echo "$ssl_full" | sed -e "s!^$scripts_dir/!!")
+    local int_file=$(echo "$ssl_file" | sed -e 's!\.[Ss][Ss][Ll]$!\.int!')
+    local log_file=$ssl_file.log
+    local ssl_prefix=
+
+    # ssl compiler is too dumb to understand paths and always checks current directory
+    cd $ssl_dir
+    $compile_exe -q -n -l -p -s -O2 $ssl_file -o $int_file > $log_file
+
+    # clean unwanted lines from log
+    sed -i '/^$/d' $log_file
+    sed -i '/^Compiling /d' $log_file
+    sed -i '/THERE WERE ERRORS/g' $log_file
+
+    if [ -f "$int_file" ]; then
+       ssl_prefix=WARNING
+       rm $int_file
+    else
+       ssl_prefix=ERROR
+       exit_val=1
+    fi
+
+    # if log exists and not empty
+    if [ -s "$log_file" ]; then
+       echo ::group::[$ssl_prefix] $ssl_show
+       cat $log_file
+       echo ::endgroup::
+    fi
+    rm $log_file
+done
 
 echo exit $exit_val
 exit $exit_val
