2445724f63a8cd27384b65f220c1aded1ab73585
Wipe <wipe.git@gmail.com>
2019-11-05 09:50:01 +0100

    - use slightly different naming for rotators-compiled sfall   generic "dev" replaced with "develop", which (i hope!) should   clarify that .dll is built from sfall's "develop" branch, and is not   built neither Debug or DevXP configurations
      sfall used in mod is built exactly in same way as official one
      (using ReleaseXP configuration), but we are too young and pretty
      to wait for official releases :)

--- Tools/ddraw.dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-MOD_DIR="$(git rev-parse --show-toplevel)/Fallout2/Fallout1in2"
-
-curl -L "https://ci.appveyor.com/api/projects/rotators/sfall/artifacts/ddraw.dll?job=Configuration:%20ReleaseXP&branch=develop&pr=false" -o "$MOD_DIR/ddraw.dev.dll"
