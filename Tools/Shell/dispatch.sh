6a2526d440783f97af593511f30d9e7dc9623930
Wipe <wipe.git@gmail.com>
2019-11-22 05:27:47 +0100

    - dispatch.sh can send client_payload now   values are sent as strings, sending bool/number doesn't work (yet?)   requires jq, mayble later i'll get rid of it

--- Tools/Shell/dispatch.sh
+++ Tools/Shell/dispatch.sh
@@ -6,15 +6,47 @@ readonly repo="rotators/Fo1in2"
 
 event="$1"
 if [ -z "$event" ]; then
-   echo "Usage: dispatch.sh [event]"
+   echo "Usage:    dispatch.sh [event] (payload1:value1) (payloadN:valueN)"
+   echo
+   echo "Example:  dispatch.sh my-event"
+   echo "Example:  dispatch.sh my-event my-key:my-value"
+   echo
    exit 1
 fi
 
-known=(schedule schedule-push)
+payload=
+for tmpayload in ${@:2}; do
+	if [[ "$tmpayload" =~ : ]]; then
+		key="$(echo "$tmpayload" | awk -F : '{print $1}')"
+		val="$(echo "$tmpayload" | awk -F : '{print $2}')"
 
-echo "Sending dispatch"
-echo "  event_type: $event"
+		if   [ "$key" == "" ]; then
+			echo "Invalid payload key, you doofus!"
+			exit 1
+		elif [ "$val" == "" ]; then
+			echo "Invalid payload value, you doofus!"
+			exit 1
+		fi
+
+		payload="$payload,\"$key\":\"$val\""
+		payload=${payload#,}
+	else
+		echo "Invalid payload, you doofus!"
+		exit 1
+	fi
+done
+
+if [ -n "$payload" ]; then
+	data=$(jq -n --arg event "$event" --argjson payload "{$payload}" '{"event_type": $event, "client_payload": $payload}')
+else
+	data=$(jq -n --arg event "$event" '{"event_type": $event}')
+fi
+
+echo "dispatch"
+echo "$data"
 echo
+
+known=(schedule schedule-push)
 
 if [[ ! " ${known[@]} " =~ " ${event} " ]]; then
 	echo "WARNING: unknown event '$event'"
@@ -34,4 +66,4 @@ if [ -z "$username" ]; then
 	exit 1
 fi
 
-$winpty curl -i -u "$username" -H "Accept: application/vnd.github.everest-preview+json" -H "Content-Type: application/json" https://api.github.com/repos/$repo/dispatches --data "{\"event_type\": \"$event\"}"
+$winpty curl -i -u "$username" -H "Accept: application/vnd.github.everest-preview+json" -H "Content-Type: application/json" https://api.github.com/repos/$repo/dispatches --data "$data"
