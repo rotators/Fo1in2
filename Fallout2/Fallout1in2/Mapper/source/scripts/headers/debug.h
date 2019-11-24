1b5bf961293aeea920e80917446d0934288c3608
Wipe <wipe.git@gmail.com>
2019-11-24 02:28:11 +0100

    DEBUGMSG() introduced by #10 replaced with debug() - prevent creating micromanaging mess   it might be good for testing just one script, but then you have to   worry about always recompiling script the same way if you want to   keep seeing debug message; in a long run, not worth it - daily recompilation will remove message due to missing -m switch   spoiler: introducing per-file compiler switches won't happen - it's good solution to keep script clean out of debug crap   for player, no doubt about that; we might go back to this WHEN/IF   script is ready to be used by other mods. iirc @Lexx2k mentioned   it should be possible some day, but it's a veeery distant future

--- Fallout2/Fallout1in2/Mapper/source/scripts/headers/debug.h
+++ Fallout2/Fallout1in2/Mapper/source/scripts/headers/debug.h
@@ -6,11 +6,4 @@
 #define debug_mode  (global_var(GVAR_ENABLE_DEBUG_MESSAGES) == 1)
 #define debug(x)    if debug_mode then display_msg(x)
 
-// Macros for debugging messages
-#ifdef DEBUG
-#define DEBUGMSG(x)       debug_msg(x);
-#else
-#define DEBUGMSG(x)       //nothing
-#endif
-
 #endif // DEBUG_H //
