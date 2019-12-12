5021a81a077119ff3de2b426b53d9febb0095489
Lexx2k <lexxnet@web.de>
2019-12-12 16:03:49 +0100

    Turned some copy&paste into an array.

--- Fallout2/Fallout1in2/Mapper/source/scripts/headers/brotherhood.h
+++ Fallout2/Fallout1in2/Mapper/source/scripts/headers/brotherhood.h
@@ -19,7 +19,8 @@
 #define set_scout_quest_completed       if scout_quest_active then set_global_var(GVAR_SCOUTED_NORTH_BRO, 2)
 
 // Piss off important people and see yourself out
-#define dude_kicked_out                 set_global_var(GVAR_QUEST_BROHOOD_1_JOIN_THEM, 5);    \
+#define get_dude_kicked_out             (global_var(GVAR_QUEST_BROHOOD_1_JOIN_THEM) >= 5)
+#define set_dude_kicked_out             set_global_var(GVAR_QUEST_BROHOOD_1_JOIN_THEM, 5);    \
                                         set_global_var(GVAR_MARK_BROTHER_2, 0);               \
                                         set_global_var(GVAR_MARK_BROTHER_3, 0);               \
                                         set_global_var(GVAR_MARK_BROTHER_4, 0);               \
