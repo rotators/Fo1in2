7728e13a7182b4c436329a560e5670b7acb56cc2
Wipe <wipe.git@gmail.com>
2019-12-13 15:30:07 +0100

    - gha ddraw script improvements (.ini handling)   awk one-liners changed into customizable template   'get' command allows to customize outpur format from command line   'set' command allows to change settings with spaces around '='   'list' command added  - prints whole .ini section - gha schedule uses ddraw script to dump updated VersionString - gha package dumps sections which has been modified

--- .github/workflows/scripts/ddraw.sh
+++ .github/workflows/scripts/ddraw.sh
@@ -18,15 +18,22 @@ function Usage()
     echo "COMMANDS"
     echo ""
     echo "  get               prints setting in .ini file"
-    echo "    --ini=[path]    path to ddraw.ini"
+    echo "    --ini=[path]    path to .ini file"
     echo "    --cfg=[name]    section name"
     echo "    --key=[name]    key name"
+    echo "    --fmt=[format]  (optional) use specific format for output"
     echo ""
     echo "  set               changes setting in .ini file"
-    echo "    --ini=[path]    path to ddraw.ini"
+    echo "    --ini=[path]    path to .ini file"
     echo "    --cfg=[name]    section name"
     echo "    --key=[name]    key name"
     echo "    --val=[string]  key value"
+    echo "    --with-space    (optional) adds spaces around '='"
+    echo ""
+    echo "  list              prints section settings in .ini file"
+    echo "    --ini=[path]    path to .ini file"
+    echo "    --cfg=[name]    section name"
+    echo "    --section-name  (optional) adds section name before output"
     echo ""
     echo "  get-dll-appveyor  downloads ddraw.dll"
     echo "    --dll=[path]    output filename"
@@ -41,7 +48,7 @@ function Usage()
     echo "  set-mod-version   updates setting 'VersionString'"
     echo "    --ini=[path]    path to ddraw.ini"
     echo "    --mod=[string]  mod version string; replacements: @COMMITS@"
-    echo "    --bump          (optional) increase minor mod version by 1"
+    echo "    --bump          (optional) increases @COMMITS@ value by 1"
     echo ""
 
     if [ -n "$error" ]; then
@@ -53,20 +60,81 @@ function Usage()
 }
 
 # here be dragon
-# awk -v section="SectionName" -v key="SettingName" 'match($0,/^[\t\ ]*\[(.+)\]/,re) {curr=re[1]; next} match($0,/^[\t\ ]*(.+)[\t\ ]*=[\t\ ]*(.+)/,line) {gsub("\r",""); if(curr!=section || line[1]!=key) next; print NR"|"curr"|"line[1]"|"line[2]}' FileName.ini
+# based on https://stackoverflow.com/a/54597545/11998612
+function IniParse()
+{
+    local ini='\
+# find and save current section                         \
+match( $0, /^[\t\ ]*\[(.+)\]/, re )                     \
+{                                                       \
+    in_section=re[1];                                   \
+    next;                                               \
+}                                                       \
+                                                        \
+# find and print current setting                        \
+match( $0, /^[\t\ ]*(.+)[\t\ ]*=[\t\ ]*(.+)/, in_key )  \
+{                                                       \
+    # skip comments                                     \
+    if( match( in_key[1], /^[#;]/ ))                    \
+        next;                                           \
+                                                        \
+    # normalize line ending                             \
+    gsub( "\r", "" );                                   \
+                                                        \
+    # normalize setting name/value                      \
+    sub( /[\t ]+$/, "", in_key[1] );                    \
+    sub( /^[\t ]+/, "", in_key[2] );                    \
+                                                        \
+    # skip unwanted entries                             \
+    if( ___IF___ )                                      \
+        next;                                           \
+                                                        \
+    # strip trailing comment                            \
+    sub( /[\t ]*[;#].*$/, "", in_key[2] );              \
+                                                        \
+    # print results                                     \
+    #   in_section  current section name                \
+    #   in_key[1]   current setting name                \
+    #   in_key[2]   current setting value               \
+    print ___PRINT___                                   \
+}'
+
+    # allow changing which parts should be printed
+    if [ -n "$1" ]; then
+       local _print=$1
+    else
+       local _print='"["in_section"]", in_key[1], "=", in_key[2]'
+    fi
+
+    # allow changing which lines should be skipped
+    if [ -n "$2" ]; then
+       local _if=$2
+    else
+       local _if='in_section != section || in_key[1] != key'
+    fi
+
+    local ini=$(echo "$ini" | sed -e "s@___PRINT___@${_print}@g" -e "s@___IF___@${_if}@g")
+
+    echo "$ini"
+}
 
 function Get()
 {
     local ini=$1
     local cfg=$2
     local key=$3
+    local fmt=$4
 
     [ -z "$ini" ] && Usage ".ini filename not set"
     [ -z "$cfg" ] && Usage ".ini section not set"
     [ -z "$key" ] && Usage ".ini key not set"
     [ ! -r $ini ] && Usage "cannot read '$ini'"
 
-    awk -v section="$cfg" -v key="$key" 'match($0,/^[\t\ ]*\[(.+)\]/,re) {curr=re[1]; next} match($0,/^[\t\ ]*(.+)[\t\ ]*=[\t\ ]*(.+)/,line) {gsub("\r",""); if(curr!=section || line[1]!=key) next; print line[2]}' $ini
+    if [ -z "$fmt" ]; then
+       local fmt='in_key[2]'
+    fi
+
+    awk -v section="$cfg" -v key="$key" "$(IniParse "$fmt")" $ini
 }
 
 function Set()
@@ -75,6 +143,7 @@ function Set()
     local cfg=$2
     local key=$3
     local val=$4
+    local int=$5
 
     [ -z "$ini" ] && Usage ".ini filename not set"
     [ -z "$cfg" ] && Usage ".ini section not set"
@@ -83,20 +152,43 @@ function Set()
     [ ! -r $ini ] && Usage "cannot read '$ini'"
     [ ! -w $ini ] && Usage "cannot write '$ini'"
 
+    if [ -n "$int" ] && [ $int -eq 1 ]; then
+       local space=" "
+    else
+       local space=""
+    fi
+
     # get line number with [section]->key setting
-    local line=$(awk -v section="$cfg" -v key="$key" 'match($0,/^[\t\ ]*\[(.+)\]/,re) {curr=re[1]; next} match($0,/^[\t\ ]*(.+)[\t\ ]*=[\t\ ]*(.+)/,line) {gsub("\r",""); if(curr!=section || line[1]!=key) next; print NR}' $ini) #'
+    local line=$(awk -v section="$cfg" -v key="$key" "$(IniParse 'NR')" $ini) #"
 
     if [ -z "$line" ]; then
-       echo "ERROR: [$cfg]->$key not found";
+       echo "ERROR: setting [$cfg]->$key not found";
        exit 1
     fi
 
     dos2unix -q $ini
 
     # rewrite line with new setting
-    sed -ri "${line}s!^.+!$key=$val!" $ini
+    sed -ri "${line}s!^.+!${key}${space}=${space}$val!" $ini
 
     unix2dos -q $ini
+}
+
+function List()
+{
+    local ini=$1
+    local cfg=$2
+    local int=$3
+
+    [ -z "$ini" ] && Usage ".ini filename not set"
+    [ -z "$cfg" ] && Usage ".ini section not set"
+    [ ! -r $ini ] && Usage "cannot read '$ini'"
+
+    if [ -n "$int" ] && [ $int -eq 1 ]; then
+       echo "[$cfg]"
+    fi
+
+    awk -v section="$cfg" "$(IniParse 'in_key[1]" = "in_key[2]' 'in_section != section')" $ini
 }
 
 # download ddraw.dll compiled from 'develop' branch (no changes compared to original sfall repository)
@@ -114,7 +206,7 @@ function GetDllAppVeyor()
 # using 7zip which is probably most portable way to do that
 # for reference:
 #   powershell -command "(Get-Item ddraw.dll).VersionInfo.ProductVersion"
-#   exiftool ddraw.dll | egrep '^File Version[\t\ ]+:' | awk '{print $4}'
+#   exiftool ddraw.dll | egrep '^File Version[\t ]+:' | awk '{print $4}'
 function GetDllVersion()
 {
     local dll=$1
@@ -153,14 +245,14 @@ function SetModVersion()
 {
     local ini=$1
     local mod=$2
-    local bump=$3
+    local int=$3
 
     [ -z "$ini" ] && Usage ".ini filename not set"
     [ -z "$mod" ] && Usage "mod version not set"
 
     local commits=$(git rev-list HEAD --count)
 
-    if [ $bump -eq 1 ]; then
+    if [ -n "$int" ] && [ $int -eq 1 ]; then
        local commits=$((commits+1))
     fi
 
@@ -171,11 +263,13 @@ function SetModVersion()
 
 option_cfg=
 option_dll=
+option_fmt=
 option_ini=
 option_key=
 option_mod=
 option_val=
-option_bump=0
+
+option_int=0
 
 if   [ -z "$command" ]; then
      Usage
@@ -184,16 +278,25 @@ elif [ "$command" == "get" ]; then
          [[ "$option" =~ ^--ini=(.+)$ ]] && option_ini=${BASH_REMATCH[1]}
          [[ "$option" =~ ^--cfg=(.+)$ ]] && option_cfg=${BASH_REMATCH[1]}
          [[ "$option" =~ ^--key=(.+)$ ]] && option_key=${BASH_REMATCH[1]}
+         [[ "$option" =~ ^--fmt=(.+)$ ]] && option_fmt=${BASH_REMATCH[1]}
      done
-     Get "$option_ini" "$option_cfg" "$option_key"
+     Get "$option_ini" "$option_cfg" "$option_key" "$option_fmt"
 elif [ "$command" == "set" ]; then
      for option in "${@:2}"; do
          [[ "$option" =~ ^--ini=(.+)$ ]] && option_ini=${BASH_REMATCH[1]}
          [[ "$option" =~ ^--cfg=(.+)$ ]] && option_cfg=${BASH_REMATCH[1]}
          [[ "$option" =~ ^--key=(.+)$ ]] && option_key=${BASH_REMATCH[1]}
          [[ "$option" =~ ^--val=(.+)$ ]] && option_val=${BASH_REMATCH[1]}
+         [[ "$option" =~ ^--with-space$ ]] && option_int=1
      done
-     Set "$option_ini" "$option_cfg" "$option_key" "$option_val"
+     Set "$option_ini" "$option_cfg" "$option_key" "$option_val" "$option_int"
+elif [ "$command" == "list" ]; then
+     for option in "${@:2}"; do
+         [[ "$option" =~ ^--ini=(.+)$ ]] && option_ini=${BASH_REMATCH[1]}
+         [[ "$option" =~ ^--cfg=(.+)$ ]] && option_cfg=${BASH_REMATCH[1]}
+         [[ "$option" =~ ^--section-name ]] && option_int=1
+     done
+     List "$option_ini" "$option_cfg" "$option_int"
 elif [ "$command" == "get-dll-appveyor" ]; then
      for option in "${@:2}"; do
          [[ "$option" =~ ^--dll=(.+)$ ]] && option_dll=${BASH_REMATCH[1]}
@@ -214,9 +317,9 @@ elif [ "$command" == "set-mod-version" ]; then
      for option in "${@:2}"; do
          [[ "$option" =~ ^--ini=(.+)$ ]] && option_ini=${BASH_REMATCH[1]}
          [[ "$option" =~ ^--mod=(.+)$ ]] && option_mod=${BASH_REMATCH[1]}
-         [[ "$option" =~ ^--bump$ ]] && option_bump=1
+         [[ "$option" =~ ^--bump$ ]] && option_int=1
      done
-     SetModVersion "$option_ini" "$option_mod" "$option_bump"
+     SetModVersion "$option_ini" "$option_mod" "$option_int"
 else
      Usage "unknown command '$command'"
 fi
