2045e1d4fbc8d01564555e321921ae2be22e8c62
Markus <ghsthack@gmail.com>
2019-12-15 07:23:39 +0100

    The first FO2 debugger, probably.

---
+++ Tools/Debugger/resource.h
@@ -0,0 +1,27 @@
+#define IDC_MYICON                      2
+#define IDD_DEBUGGER_DIALOG             102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDI_DEBUGGER                    107
+#define IDI_SMALL                       108
+#define IDC_DEBUGGER                    109
+#define IDR_MAINFRAME                   128
+#define ID_FILE_ATTACH                  32771
+#define IDC_STATIC                      -1
+
+// Menu 
+#define IDM_ATTACH                      103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32772
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
