a24ea2139169eb2d2e18a2a1d6ac817b0f3b98b7
Markus <ghsthack@gmail.com>
2020-01-02 02:26:14 +0100

    Removing a bunch of redundant code
    - Engine project no longer needed.
    - dll is stripped to bare minimum if we need to experiment with something outside of sfall.

--- Reversing/Engine/Source/dude.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace dude {
-	void __declspec() hook_move();
-	void __declspec() hook_run();
-	void run(int actionPoints);
-	void move(int actionPoints);
-	int check_move(int actionPoints);
-}
