dc7fa8d4aabd46fa604df0b0291052e971beae55
Wipe <wipe.git@gmail.com>
2020-01-10 06:43:34 +0100

    - mod-specific metarules header - gitignore for private global scripts

---
+++ Fallout2/Fallout1in2/Mapper/source/scripts/headers/sfall/sfall.rotators.h
@@ -0,0 +1,7 @@
+#ifndef __SFALL_ROTATORS__
+#define __SFALL_ROTATORS__
+
+#define r_tolower(val)  sfall_func1("r_tolower", val)
+#define r_toupper(val)  sfall_func1("r_toupper", val)
+
+#endif // __SFALL_ROTATORS__ //
