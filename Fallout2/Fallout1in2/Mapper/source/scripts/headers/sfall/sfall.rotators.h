97e2ba305ac9cbdb62be9279cfe4a71f5f66ed0b
Mayck <hotovec.michal@gmail.com>
2020-01-12 10:26:12 +0100

    Added r_set_hotspot_title macro for updating hotspot title for worldmap zones.

--- Fallout2/Fallout1in2/Mapper/source/scripts/headers/sfall/sfall.rotators.h
+++ Fallout2/Fallout1in2/Mapper/source/scripts/headers/sfall/sfall.rotators.h
@@ -3,5 +3,6 @@
 
 #define r_tolower(val)  sfall_func1("r_tolower", val)
 #define r_toupper(val)  sfall_func1("r_toupper", val)
+#define r_set_hotspot_title(zoneX, zoneY, msg)         sfall_func3("r_set_hotspot_title", zoneX, zoneY, msg)
 
 #endif // __SFALL_ROTATORS__ //
