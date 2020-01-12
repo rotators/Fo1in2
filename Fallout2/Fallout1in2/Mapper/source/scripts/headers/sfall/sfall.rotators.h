e5fc9bcb711d837d2f7d5d5978391e74e43b62b1
Lexx2k <lexxnet@web.de>
2020-01-12 11:25:21 +0100

    - Added: Zones around the Glow now read "Rad. Mountains" / "Rad. Desert" (can't show full text length right now). - Some file cleanup.

--- Fallout2/Fallout1in2/Mapper/source/scripts/headers/sfall/sfall.rotators.h
+++ Fallout2/Fallout1in2/Mapper/source/scripts/headers/sfall/sfall.rotators.h
@@ -1,8 +1,12 @@
 #ifndef __SFALL_ROTATORS__
 #define __SFALL_ROTATORS__
 
-#define r_tolower(val)  sfall_func1("r_tolower", val)
-#define r_toupper(val)  sfall_func1("r_toupper", val)
-#define r_set_hotspot_title(zoneX, zoneY, msg)         sfall_func3("r_set_hotspot_title", zoneX, zoneY, msg)
+// Check if current ddraw.dll build is from Rotators
+#define sfall_rotators  (read_byte(0x410003) == 0xF4 and sfall_func1("metarule_exist", "rotators"))
+
+// Rotators sfall_funcX macros
+#define r_tolower(val)                          sfall_func1("r_tolower", val)
+#define r_toupper(val)                          sfall_func1("r_toupper", val)
+#define r_set_hotspot_title(zoneX, zoneY, msg)  sfall_func3("r_set_hotspot_title", zoneX, zoneY, msg)
 
 #endif // __SFALL_ROTATORS__ //
