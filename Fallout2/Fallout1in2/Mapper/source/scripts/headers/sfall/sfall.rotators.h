57769a5dbc498a840335da05ba41177da4bfdb3f
Wipe <wipe.git@gmail.com>
2020-01-16 13:37:47 +0100

    sfall_func() tweak

--- Fallout2/Fallout1in2/Mapper/source/scripts/headers/sfall/sfall.rotators.h
+++ Fallout2/Fallout1in2/Mapper/source/scripts/headers/sfall/sfall.rotators.h
@@ -2,15 +2,14 @@
 #define __SFALL_ROTATORS__
 
 // https://stackoverflow.com/a/44291052/11998612 + tweaks
-#define __sfall_expand(x)                          x
 #define __sfall_prefix(...)                        0,__VA_ARGS__
 #define __sfall_lastof(x,a,b,c,d,e,f,g,h,i,j,...)  j
-#define __sfall_cntarg(...)                        __sfall_expand(__sfall_lastof(__VA_ARGS__,8,7,6,5,4,3,2,1,0,0))
+#define __sfall_cntarg(...)                        __sfall_lastof(__VA_ARGS__,8,7,6,5,4,3,2,1,0,0)
 #define __sfall_numarg(...)                        __sfall_cntarg(__sfall_prefix(__VA_ARGS__))
 #define __sfall_merge(left,right)                  left ## right
 #define __sfall_func(count)                        __sfall_merge(sfall_func,count)
 
-// automagically counts number of arguments and selects matching sfall_funcX
+// automagically counts number of arguments and selects matching sfall_funcX when script is preprocessed
 // if more than 9 arguments is used, sfall_func9 is selected
 #define sfall_func(...)   __sfall_func(__sfall_numarg(__VA_ARGS__))(__VA_ARGS__)
 
