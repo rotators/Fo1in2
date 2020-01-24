03d317a2b5491f5945f4892477f6c38dabda028e
Antalaskaya <antalaskaya@gmail.com>
2020-01-24 04:22:41 +0000

    Progress every day - sfall-rotators update (v4.2.3, rotators/sfall@08e83c45c44cd7f7840af8dc5f24c955f9993792) - ModdersPack update (phobos2077/sfall@27233d19a28f54acd892224ba5f0180f1cceaf52) - Configuration refresh

--- Tools/ModdersPack/scripting/headers/sfall.h
+++ Tools/ModdersPack/scripting/headers/sfall.h
@@ -89,7 +89,7 @@
 #define WINTYPE_DIALOG       (1)
 #define WINTYPE_PIPBOY       (2)
 #define WINTYPE_WORLDMAP     (3)
-#define WINTYPE_MAINIFACE    (4) // the interface bar
+#define WINTYPE_IFACEBAR     (4) // the interface bar
 #define WINTYPE_CHARACTER    (5)
 #define WINTYPE_SKILLDEX     (6)
 #define WINTYPE_ESCMENU      (7) // escape menu
@@ -287,8 +287,8 @@
 #define get_flags(obj)                                  sfall_func1("get_flags", obj)
 #define get_ini_section(file, sect)                     sfall_func2("get_ini_section", file, sect)
 #define get_ini_sections(file)                          sfall_func1("get_ini_sections", file)
-#define get_interface_xpos(winType)                     sfall_func2("get_window_attribute", winType, 0)
-#define get_interface_ypos(winType)                     sfall_func2("get_window_attribute", winType, 1)
+#define get_interface_x(winType)                        sfall_func2("get_window_attribute", winType, 0)
+#define get_interface_y(winType)                        sfall_func2("get_window_attribute", winType, 1)
 #define get_inven_ap_cost                               sfall_func0("get_inven_ap_cost")
 #define get_map_enter_position                          sfall_func0("get_map_enter_position")
 #define get_metarule_table                              sfall_func0("get_metarule_table")
