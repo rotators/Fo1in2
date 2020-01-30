162c2955efe8c15fd01d0085dbe4c0eaf91eb3c7
Wipe <wipe.git@gmail.com>
2020-01-30 03:44:03 +0100

    - gha schedule no damage control during compilation step   if compilation fails, whole workflow fails as well - gha schedule "formats" script   currently tweaks #include directives only

---
+++ .github/workflows/scripts/format-ssl.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+for option in $@; do
+    [[ "$option" =~ ^--debug$ ]] && set -x
+done
+
+# default options
+option_scripts_dir="Fallout2/Fallout1in2/Mapper/source/scripts"
+
+# user options
+for option in "$@"; do
+    # --scripts-dir=some/directory
+    # path to .ssl files directory
+    [[ "$option" =~ ^--scripts-dir=([A-Za-z0-9_\.\/]+)$ ]] && option_scripts_dir=${BASH_REMATCH[1]}
+done
+
+readonly scripts_dir="$(pwd)/$option_scripts_dir"
+
+if [ ! -d "$scripts_dir" ]; then
+   echo "[ERROR] Cannot find scripts directory : $scripts_dir"
+   exit 1
+fi
+
+for ssl_full in $(/usr/bin/find $scripts_dir -type f -name '*.[Ss][Ss][Ll]' | sort); do
+    sed -ri 's!#include "..[\\/]headers[\\/]!#include "!' $ssl_full
+    sed -ri 's!#include "maps\\!#include "maps/!' $ssl_full
+    sed -ri 's!#include "sfall\\!#include "sfall/!' $ssl_full
+done
