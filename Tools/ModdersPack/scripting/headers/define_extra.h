c42bb5feccb1432401e1ccdc3491e083f7a99f10
Antalaskaya <antalaskaya@gmail.com>
2020-02-03 04:24:46 +0000

    Progress every day - sfall-rotators update (v4.2.3, rotators/sfall@6749eecd333ad9099d6e7048a524f3d2e5dbb4cc) - ModdersPack update (phobos2077/sfall@1f1c39a4bdff0f22b7b608300f635c4f4398be51) - Scripts recompilation - Configuration refresh - News update

--- Tools/ModdersPack/scripting/headers/define_extra.h
+++ Tools/ModdersPack/scripting/headers/define_extra.h
@@ -407,7 +407,7 @@
 #define OBJ_DATA_COMBAT_STATE       (0x3C) // flags: 1 - combat, 2 - target is out of range, 4 - flee
 #define OBJ_DATA_CUR_ACTION_POINT   (0x40)
 #define OBJ_DATA_DAMAGE_LAST_TURN   (0x48)
-#define OBJ_DATA_WHO_HIT_ME         (0x54)
+#define OBJ_DATA_WHO_HIT_ME         (0x54) // current target of the critter
 
 // compute attack result data offsets
 #define C_ATTACK_SOURCE             (0x00)
