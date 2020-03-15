27eae7249792450daafeab33f872d0a702dc9edf
Lexx2k <lexxnet@web.de>
2020-03-15 18:45:05 +0100

    Brotherhood of Steel - Added: Alternative item rewards from Talus and Mathia can now be toggled in fo1_settings.ini file (enabled by default). Laser Pistol -> Magneto-Laser Pistol Power Fist -> Mega Power Fist Ripper -> Super Sledgehammer

--- Fallout2/Fallout1in2/Mapper/source/scripts/headers/fo1.h
+++ Fallout2/Fallout1in2/Mapper/source/scripts/headers/fo1.h
@@ -35,6 +35,7 @@
 #define fo1in2_cave_maps_enabled        (global_var(GVAR_ENABLE_CAVE_MAPS) > 0)
 #define fo1in2_fo2_critters_enabled     (global_var(GVAR_ENABLE_FO2_CRITTERS) > 0)
 #define fo1in2_classic_wm_enabled       (global_var(GVAR_ENABLE_CLASSIC_WM) > 0)
+#define fo1in2_alt_bos_reward_enabled   (global_var(GVAR_ENABLE_ALTERNATIVE_BOS_REWARD) > 0)
 
 #define fixt_enabled                    (global_var(GVAR_FIXT_ENABLED) == 1)
 #define fixt_disabled                   not(fixt_enabled)
