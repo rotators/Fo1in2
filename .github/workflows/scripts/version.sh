fdf01b00b87320702e9e8055c5f6f26e1e1073d8
Wipe <wipe.git@gmail.com>
2020-06-02 22:14:31 +0200

    gha single point for updating mod version

---
+++ .github/workflows/scripts/version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+MOD_VERSION="1.2"
+
+
+MOD_COMMITS=$(git rev-list HEAD --count)
+
+if [ "$2" == "--bump" ]; then
+   MOD_COMMITS=$((MOD_COMMITS+1))
+fi
+
+MOD_VERSION_LONG="${MOD_VERSION}.${MOD_COMMITS}"
+
+[ "$1" == "game"  ]   && echo "FALLOUT ET TU v${MOD_VERSION_LONG}" && exit
+[ "$1" == "release" ] && echo "Fallout1in2 v${MOD_VERSION_LONG}"   && exit
+[ "$1" == "tag" ]     && echo "v${MOD_VERSION_LONG}"               && exit
+
+echo ""
+echo "USAGE:  $(basename $0) <game|release|tag> [--bump]"
+echo ""
+exit 1
