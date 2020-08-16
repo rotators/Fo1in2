06de910a2183ea6df8c10d8fc097a546a9cb7d0a
Wipe <wipe.git@gmail.com>
2020-08-16 13:01:26 +0200

    Update version.sh

--- .github/workflows/scripts/version.sh
+++ .github/workflows/scripts/version.sh
@@ -16,6 +16,8 @@ MOD_VERSION_LONG="${MOD_VERSION}.${MOD_COMMITS}"
 [ "$1" == "release" ] && echo "Fallout1in2 v${MOD_VERSION_LONG}"   && exit
 [ "$1" == "tag" ]     && echo "v${MOD_VERSION_LONG}"               && exit
 
+[ "$1" == "raw" ]     && echo "${MOD_VERSION}"                     && exit
+
 echo ""
 echo "USAGE:  $(basename $0) <game|release|tag> [--bump]"
 echo ""
