aeee07ada2db18f7e40b8f9064650aad865d47ab
Antalaskaya <antalaskaya@gmail.com>
2020-09-02 04:14:07 +0000

    Progress every day - ModdersPack update (phobos2077/sfall@09453823690e7771f09dcf30c4455aa0fc6708c9) - Configuration refresh - News update

--- Tools/ModdersPack/scripting/headers/define_extra.h
+++ Tools/ModdersPack/scripting/headers/define_extra.h
@@ -458,4 +458,18 @@
 #define C_ATTACK_KNOCKBACK_VALUE5   (0xB0)
 #define C_ATTACK_KNOCKBACK_VALUE6   (0xB4)
 
+/* Playback mode defines for the soundplay function */
+#define soundraw        (0x80000000) // sfall flag
+#define Stereo8bit      (soundstereo)
+#define Stereo8bitLoop  (soundstereo bwor soundloop)
+#define Mono16bit       (sound16bit)
+#define Mono16bitLoop   (sound16bit bwor soundloop)
+#define Stereo16bit     (soundstereo bwor sound16bit)
+#define Stereo16bitLoop (soundstereo bwor sound16bit bwor soundloop)
+
+// Volume reduction: 0x0000XXXX - min, 0x7FFFXXXX - mute
+#define SoundVolume25   (0x20000000)
+#define SoundVolumeHalf (0x40000000)
+#define SoundVolume75   (0x60000000)
+
 #endif // DEFINE_EXTRA_H
