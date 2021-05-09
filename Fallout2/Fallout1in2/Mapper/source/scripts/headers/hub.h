e7db538b7a7835c2786a0dc79609da4b3e4deece
Lexx2k <lexxnet@web.de>
2021-05-09 22:28:52 +0200

    Junktown - Fixed: Gizmo would not react to the player trying to steal from him after certain dialog choices. - Fixed: If Kenji is about to attack Killian and the player leaves the map, he will now be removed (if the player returns after days, he would still be there. Now he is killed off-screen by the guards). - Fixed: If Killian gets killed while fighting Gizmo, the player would get the experience points and Junktown becomes hostile. - Fixed: If Lars in Junktown gives the player 15 seconds to "leave and never come back", he would start combat no matter where on the map the player is. - Fixed: Shady Sands becomes hostile to the player, even if he kills Tandi outside of town.

---
+++ Fallout2/Fallout1in2/Mapper/source/scripts/headers/hub.h
@@ -0,0 +1,18 @@
+/*
+
+    The Hub generic defines
+
+*/
+
+#ifndef HUB_H
+#define HUB_H
+
+/************************************************
+    Generic Defines
+************************************************/
+
+// Timer for the conversation between Guido and Leon at the FLC
+#define TIMER_CONVERSATION     (6) // orig. 3
+
+
+#endif // HUB_H
