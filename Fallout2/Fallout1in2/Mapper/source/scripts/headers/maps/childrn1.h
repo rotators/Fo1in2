576a03dae561fc1a124327efdfaf87506713fc6f
Lexx2k <lexxnet@web.de>
2021-05-17 23:59:18 +0200

    Added missing childrn1/2 header files.

---
+++ Fallout2/Fallout1in2/Mapper/source/scripts/headers/maps/childrn1.h
@@ -0,0 +1,24 @@
+/*
+
+   .h Title
+
+*/
+
+#ifndef MAPCHILDRN1_H
+#define MAPCHILDRN1_H
+
+/************************************************
+    Generic Defines
+************************************************/
+
+// All Map Vars need to start w/ MVAR_
+#define MVAR_Fixt_PLACEHOLDER_0     (0)  Adding for ass-covering and future-proofing
+#define MVAR_Fixt_PLACEHOLDER_1     (1)  Adding for ass-covering and future-proofing
+#define MVAR_Fixt_PLACEHOLDER_2     (2)  Adding for ass-covering and future-proofing
+#define MVAR_Fixt_PLACEHOLDER_3     (3)  Adding for ass-covering and future-proofing
+#define MVAR_Fixt_PLACEHOLDER_4     (4)  Adding for ass-covering and future-proofing
+#define MVAR_Fixt_PLACEHOLDER_5     (5)
+#define MVAR_Fixt_PLACEHOLDER_6     (6)
+#define MVAR_BOS_FLOAT              (7)
+
+#endif // MAPCHILDRN1_H
