4f43cbe68b0dfd2e8f53d87991754737adb5ce0e
Lexx2k <lexxnet@web.de>
2021-05-18 09:19:11 +0200

    Fix.

--- Fallout2/Fallout1in2/Mapper/source/scripts/headers/maps/childrn1.h
+++ Fallout2/Fallout1in2/Mapper/source/scripts/headers/maps/childrn1.h
@@ -12,11 +12,11 @@
 ************************************************/
 
 // All Map Vars need to start w/ MVAR_
-#define MVAR_Fixt_PLACEHOLDER_0     (0)  Adding for ass-covering and future-proofing
-#define MVAR_Fixt_PLACEHOLDER_1     (1)  Adding for ass-covering and future-proofing
-#define MVAR_Fixt_PLACEHOLDER_2     (2)  Adding for ass-covering and future-proofing
-#define MVAR_Fixt_PLACEHOLDER_3     (3)  Adding for ass-covering and future-proofing
-#define MVAR_Fixt_PLACEHOLDER_4     (4)  Adding for ass-covering and future-proofing
+#define MVAR_Fixt_PLACEHOLDER_0     (0)  // Adding for ass-covering and future-proofing
+#define MVAR_Fixt_PLACEHOLDER_1     (1)  // Adding for ass-covering and future-proofing
+#define MVAR_Fixt_PLACEHOLDER_2     (2)  // Adding for ass-covering and future-proofing
+#define MVAR_Fixt_PLACEHOLDER_3     (3)  // Adding for ass-covering and future-proofing
+#define MVAR_Fixt_PLACEHOLDER_4     (4)  // Adding for ass-covering and future-proofing
 #define MVAR_Fixt_PLACEHOLDER_5     (5)
 #define MVAR_Fixt_PLACEHOLDER_6     (6)
 #define MVAR_BOS_FLOAT              (7)
--- Fallout2/Fallout1in2/Mapper/source/scripts/headers/maps/childrn2.h
+++ Fallout2/Fallout1in2/Mapper/source/scripts/headers/maps/childrn2.h
@@ -4,18 +4,18 @@
 
 */
 
-#ifndef MAPCHILDRN1_H
-#define MAPCHILDRN1_H
+#ifndef MAPCHILDRN2_H
+#define MAPCHILDRN2_H
 
 /************************************************
     Generic Defines
 ************************************************/
 
 // All Map Vars need to start w/ MVAR_
-#define MVAR_Fixt_PLACEHOLDER_0     (0)  Adding for ass-covering and future-proofing
-#define MVAR_Fixt_PLACEHOLDER_1     (1)  Adding for ass-covering and future-proofing
-#define MVAR_Fixt_PLACEHOLDER_2     (2)  Adding for ass-covering and future-proofing
-#define MVAR_Fixt_PLACEHOLDER_3     (3)  Adding for ass-covering and future-proofing
-#define MVAR_Fixt_PLACEHOLDER_4     (4)  Adding for ass-covering and future-proofing
+#define MVAR_Fixt_PLACEHOLDER_0     (0)  // Adding for ass-covering and future-proofing
+#define MVAR_Fixt_PLACEHOLDER_1     (1)  // Adding for ass-covering and future-proofing
+#define MVAR_Fixt_PLACEHOLDER_2     (2)  // Adding for ass-covering and future-proofing
+#define MVAR_Fixt_PLACEHOLDER_3     (3)  // Adding for ass-covering and future-proofing
+#define MVAR_Fixt_PLACEHOLDER_4     (4)  // Adding for ass-covering and future-proofing
 
-#endif // MAPCHILDRN1_H
+#endif // MAPCHILDRN2_H
