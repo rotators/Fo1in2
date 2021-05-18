9f906718717871e9cca907f17f62da6004d90969
Lexx2k <lexxnet@web.de>
2021-05-18 08:45:26 +0200

    Cathedral - Tweaked: Improved the floating text code of the FoA invades at the Cathedral a bit (only one will float, not all at the same time).

--- Fallout2/Fallout1in2/Mapper/source/scripts/headers/area/cathedral.h
+++ Fallout2/Fallout1in2/Mapper/source/scripts/headers/area/cathedral.h
@@ -12,6 +12,21 @@
     Generic Defines
 ************************************************/
 
+// Cathedral invaders
+#define FOLINVAD_FLOAT_Entrance     bit_1
+#define FOLINVAD_FLOAT_Vault        bit_2
+#define FOLINVAD_FLOAT_Master       bit_3
+#define FOLINVAD_FLOAT_Exit         bit_4
+#define BROINVASD_FLOAT_Room        bit_5
+#define BROINVASD_FLOAT_Master      bit_6
+
+#define set_invad_status(x)         set_gvar_bit_on(GVAR_FOLLOWER_MACHO_STATES, x)
+#define invad_status(x)             (gvar_bit(GVAR_FOLLOWER_MACHO_STATES, x))
+
+#define ghunters_killed             (gvar_bit(GVAR_FOLLOWER_MACHO_STATES, HUNTER_FATHER_DEAD) and gvar_bit(GVAR_GECKO_HUNTER_STATUS, HUNTER_SON_DEAD))
+#define ghunters_party_killed       (gvar_bit(GVAR_FOLLOWER_MACHO_STATES, HUNTER_WAR_PARTY))
+#define ghunters_pc_knows_names     (gvar_bit(GVAR_FOLLOWER_MACHO_STATES, HUNTER_PC_KNOWS_NAMES))
+#define ghunters_know_pc_name       (gvar_bit(GVAR_FOLLOWER_MACHO_STATES, HUNTER_KNOWS_PC_NAME))
 
 #endif // CATHEDRAL_H
 
--- Fallout2/Fallout1in2/Mapper/source/scripts/headers/global.h
+++ Fallout2/Fallout1in2/Mapper/source/scripts/headers/global.h
@@ -4058,8 +4058,8 @@
 #define RESERVED_FOR_FALLOUT_FIXT               (4918)
 #define RESERVED_FOR_FALLOUT_FIXT               (4919)
 #define RESERVED_FOR_FALLOUT_FIXT               (4920)
-#define RESERVED_FOR_FALLOUT_FIXT               (4921)
-#define RESERVED_FOR_FALLOUT_FIXT               (4922)*/
+#define RESERVED_FOR_FALLOUT_FIXT               (4921)*/
+#define GVAR_FOLLOWER_MACHO_STATES              (4922)
 #define GVAR_QUEST_KILL_HARRY                   (4923)
 #define GVAR_ENABLE_EXPANDED_RADIATION          (4924)
 #define GVAR_HIGHTOWER_STATUS                   (4925)
