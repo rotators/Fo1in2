rotators/Fo1in2

commit b4601edf6caec073e1ea4bdde372a2bea4b56fcc

Shady Sands - Fixed: Aradesh in Shady Sands had a wrong reaction condition. - Fixed: Tine ("Taylor") in Adytum had a wrong reaction condition.
- General cleanup for reaction conditions.

Fallout2/Fallout1in2/Mapper/source/scripts/02shady/COOK.ssl
@@ -101,7 +101,7 @@ procedure talk_p_proc begin
    set_tma_data_generic(TMA_MSG_SHADYW);
 
    dude_look_at_self;
-   if (REP_ENEMY_SHADY_SANDS or (local_var(LVAR_reaction_level) == BAD)) then begin
+   if (REP_ENEMY_SHADY_SANDS or bad_critter_reaction) then begin
       float_msg(self_obj, message_str(SCRIPT_ENEMY, random(100, 105)), FLOAT_MSG_NORMAL);
    end
    else begin

Fallout2/Fallout1in2/Mapper/source/scripts/03junktown/ISMARC.ssl
@@ -161,7 +161,7 @@ procedure timed_event_p_proc begin
 end
 
 procedure Ismarc01 begin
-   if (local_var(LVAR_reaction_level) < NEUTRAL) then begin
+   if bad_critter_reaction then begin
       Reply(102);
    end
    else begin
@@ -175,7 +175,7 @@ end
 
 procedure Ismarc02 begin
    UpReact
-   if (local_var(LVAR_reaction_level) == BAD) then begin
+   if bad_critter_reaction then begin
       call Ismarc03;
    end
    else begin

Fallout2/Fallout1in2/Mapper/source/scripts/03junktown/VINNIE.ssl
@@ -174,7 +174,7 @@ procedure talk_p_proc begin
       end_dialogue;
    end
 
-   if (local_var(LVAR_reaction_level) == BAD) then begin
+   if bad_critter_reaction then begin
       give_xp_msg(EXP_STEAL_URN, mstr(163));
       dec_general_rep(1);
       set_local_var(LVAR_reaction_level, NEUTRAL);
@@ -343,7 +343,7 @@ end
 
 procedure Vinnie20 begin
    set_local_var(LVAR_got_reaction, NO_REACTION);
-   if (local_var(LVAR_reaction_level) == NO_REACTION) then begin
+   if horrible_critter_reaction then begin
       set_local_var(LVAR_reaction_level, BAD);
    end
    if (global_var(GVAR_IS_NEAL_DEAD) == 1) then begin

Fallout2/Fallout1in2/Mapper/source/scripts/05necropolis/GARRET.ssl
@@ -287,7 +287,7 @@ procedure reward begin
    CASH := 0;
    robbed := 0;
    display_msg(mstr(203));
-   if (local_var(LVAR_reaction_level) >= GOOD) then begin
+   if good_critter_reaction then begin
       call goodstuff;
    end
    else begin

Fallout2/Fallout1in2/Mapper/source/scripts/07bos/GENINIT.ssl
@@ -32,7 +32,7 @@ procedure start begin
    if Only_Once then begin
       Only_Once := 0;
       set_self_team(TEAM_BROTHERHOOD);
-      if (local_var(LVAR_reaction_level) < BAD) then begin
+      if horrible_critter_reaction then begin
          temp := random(BAD, NEUTRAL);
          set_local_var(LVAR_reaction_level, temp);
       end

Fallout2/Fallout1in2/Mapper/source/scripts/MAIN/IAN.ssl
@@ -471,7 +471,7 @@ procedure Ian16 begin
 end
 
 procedure Ian17 begin
-   if (local_var(LVAR_reaction_level) == BAD) then begin
+   if bad_critter_reaction then begin
       BMessage(148);
    end
    else if (local_var(LVAR_reaction_level) >= NEUTRAL) then begin

Fallout2/Fallout1in2/Mapper/source/scripts/00V13/VALTCUPL.ssl
@@ -80,7 +80,7 @@ procedure talk_p_proc begin
    get_reaction
 
    if (local_var(LVAR_HasTalkedBefore)) then begin
-      if (local_var(LVAR_reaction_level) < NEUTRAL) then begin
+      if bad_critter_reaction then begin
          start_dialog_at_node(ValtCupl07);
       end
       else begin

Fallout2/Fallout1in2/Mapper/source/scripts/02shady/ARADESH.ssl
@@ -609,7 +609,7 @@ procedure aradesh24c begin
 end
 
 procedure aradesh25 begin
-   if (local_var(LVAR_reaction) < 2) then begin
+   if bad_critter_reaction then begin
       Reply(185);
    end
    else begin

commit 3976e9c7caa3cc9366f82245cf90fccb49b2f48d

Critter Reactions - Fixed some reaction conditions that got broken with https://github.com/rotators/Fo1in2/commit/b4601edf6caec073e1ea4bdde372a2bea4b56fcc - Changed Fo2 talking head animation conditions to the values of Fo1. - Some code cleanup.

Fallout2/Fallout1in2/Mapper/source/scripts/02shady/COOK.ssl
@@ -27,6 +27,8 @@ procedure pickup_p_proc;
 
 procedure Cook01;
 
+#define LVAR_Herebefore    (4)
+
 variable tmp_hostile;
 variable initial := 0;
 variable round_counter;
@@ -71,7 +73,7 @@ end
 
 procedure damage_p_proc begin
    if source_is_dude then begin
-      set_local_var(1, 1);
+      set_local_var(LVAR_reaction_level, BAD);
       if CUR_AREA_SHADY_SANDS then begin
          set_global_var(GVAR_ENEMY_SHADY_SANDS, 1);
       end
@@ -101,11 +103,11 @@ procedure talk_p_proc begin
    set_tma_data_generic(TMA_MSG_SHADYW);
 
    dude_look_at_self;
-   if (REP_ENEMY_SHADY_SANDS or bad_critter_reaction) then begin
+   if (REP_ENEMY_SHADY_SANDS or (local_var(LVAR_reaction_level) == BAD)) then begin
       float_msg(self_obj, message_str(SCRIPT_ENEMY, random(100, 105)), FLOAT_MSG_NORMAL);
    end
    else begin
-      if (dude_stat_success(STAT_lu, 0) and (local_var(LVAR_reaction) == 0) and (dude_iq >= 4)) then begin
+      if (dude_stat_success(STAT_lu, 0) and (local_var(LVAR_Herebefore) == 0) and (dude_iq >= 4)) then begin
          start_gdialog(114, self_obj, 4, -1, -1);
          gsay_start;
          Reply(101);
@@ -116,7 +118,7 @@ procedure talk_p_proc begin
       else begin
          float_msg(self_obj, mstr(103), FLOAT_MSG_YELLOW);
       end
-      set_local_var(LVAR_reaction, 1); // Note: This is not correctly used, but the dialog is simple so it doesn't matter.
+      set_local_var(LVAR_Herebefore, 1);
    end
 end
 

Fallout2/Fallout1in2/Mapper/source/scripts/03junktown/ISMARC.ssl
@@ -67,7 +67,7 @@ procedure critter_p_proc begin
       attack(dude_obj);
    end
    else begin
-      if (local_var(4) == 0) then begin
+      if (local_var(LVAR_Sleeping) == 0) then begin
          if (self_tile != home_tile) then begin
             self_walk_to_tile(home_tile);
          end

Fallout2/Fallout1in2/Mapper/source/scripts/03junktown/VINNIE.ssl
@@ -7,6 +7,7 @@
 /* Include Files */
 #include "define.h"
 #include "area/junktown.h"
+#include "maps/junkcsno.h" // Vinnie is actually in junkkill, but he shows up in junkcsno when attacking Neal
 
 #define NAME                    SCRIPT_VINNIE
 #define TOWN_REP_VAR            (GVAR_TOWN_REP_JUNKTOWN)
@@ -58,6 +59,10 @@ procedure set_sleep_time;
 import variable Neal_ptr;
 import variable removal_ptr;
 
+#define LVAR_Herebefore    (4)
+#define LVAR_StealUrn      (5)
+#define LVAR_GaveUrn       (6)
+
 variable tmp_hostile;
 variable line160flag;
 variable line162flag;
@@ -74,7 +79,7 @@ end
 procedure timed_event_p_proc begin
    if (fixed_param == 1) then begin
       game_ui_enable;
-      if (map_var(1) != 0) then begin
+      if (map_var(MVAR_guards_left) != 0) then begin
          set_global_var(GVAR_DUDE_ENEMY_SKULZ, 1);
          float_msg(self_obj, mstr(162), FLOAT_MSG_RED);
          attack(dude_obj);
@@ -107,7 +112,7 @@ end
 
 procedure destroy_p_proc begin
    if (CUR_MAP_JUNKCSNO) then begin
-      dec_map_var(0);
+      dec_map_var(MVAR_Skulz_left);
    end
 
    if source_is_dude then begin
@@ -121,7 +126,7 @@ end
 
 procedure look_at_p_proc begin
    script_overrides;
-   if (local_var(LVAR_reaction)) then begin
+   if (local_var(LVAR_Herebefore)) then begin
       display_msg(mstr(100));
    end
    else begin
@@ -161,10 +166,10 @@ procedure talk_p_proc begin
       if (global_var(GVAR_BUST_NEAL_STATE) == 1) then begin
          call Vinnie24;
       end
-      else if (local_var(LVAR_got_reaction) == BAD) then begin
+      else if (local_var(LVAR_StealUrn) == 1) then begin
          call Vinnie19;
       end
-      else if (local_var(LVAR_reaction) == BAD) then begin
+      else if (local_var(LVAR_Herebefore) == BAD) then begin
          call Vinnie10;
       end
       else begin
@@ -174,10 +179,10 @@ procedure talk_p_proc begin
       end_dialogue;
    end
 
-   if bad_critter_reaction then begin
+   if (local_var(LVAR_GaveUrn) == 1) then begin
       give_xp_msg(EXP_STEAL_URN, mstr(163));
       dec_general_rep(1);
-      set_local_var(LVAR_reaction_level, NEUTRAL);
+      set_local_var(LVAR_GaveUrn, 2);
       if (global_var(GVAR_IS_NEAL_DEAD) == 0) then begin
          set_global_var(GVAR_BUST_NEAL_TIMER, (GAME_TIME_IN_DAYS) + 2);
       end
@@ -194,7 +199,7 @@ procedure talk_p_proc begin
 end
 
 procedure Vinnie01 begin
-   set_local_var(LVAR_reaction, 1);
+   set_local_var(LVAR_Herebefore, 1);
    Reply(103);
    NOption(104, Vinnie02, 4);
    NOption(105, Vinnie03, 4);
@@ -320,7 +325,7 @@ procedure Vinnie16 begin
 end
 
 procedure Vinnie17 begin
-   set_local_var(LVAR_got_reaction, BAD);
+   set_local_var(LVAR_StealUrn, 1);
    GMessage(146);
 end
 
@@ -342,9 +347,9 @@ procedure Vinnie19 begin
 end
 
 procedure Vinnie20 begin
-   set_local_var(LVAR_got_reaction, NO_REACTION);
-   if horrible_critter_reaction then begin
-      set_local_var(LVAR_reaction_level, BAD);
+   set_local_var(LVAR_StealUrn, 0);
+   if (local_var(LVAR_GaveUrn) == 0) then begin
+      set_local_var(LVAR_GaveUrn, 1);
    end
    if (global_var(GVAR_IS_NEAL_DEAD) == 1) then begin
       call Vinnie22;
@@ -366,7 +371,7 @@ end
 procedure Vinnie22 begin
    set_quest_join_skullz_completed;
 
-   set_local_var(LVAR_got_reaction, NO_REACTION);
+   set_local_var(LVAR_StealUrn, 0);
    set_global_var(GVAR_BUST_NEAL_STATE, 2);
    GMessage(157);
 end

Fallout2/Fallout1in2/Mapper/source/scripts/05necropolis/GARRET.ssl
@@ -43,6 +43,9 @@ procedure give_cola;
 import variable Fridge_ptr;
 import variable Garret_ptr;
 
+#define LVAR_Herebefore    (4)
+#define LVAR_Fridge        (5)
+
 variable Shotgun_ptr;
 variable Shells_ptr;
 variable Cola_ptr;
@@ -99,34 +102,34 @@ procedure critter_p_proc begin
       end
 
       if (global_var(GVAR_SIGNAL_REWARD) bwand 1) then begin
-         if (local_var(5) == 0) then begin
+         if (local_var(LVAR_Fridge) == 0) then begin
             moving := 1;
             float_msg(self_obj, mstr(114), FLOAT_MSG_NORMAL);
-            set_local_var(5, 1);
+            set_local_var(LVAR_Fridge, 1);
          end
-         else if (local_var(5) == 1) then begin
+         else if (local_var(LVAR_Fridge) == 1) then begin
             moving := 1;
             if (my_hex != target_hex) then begin
                self_walk_to_tile(target_hex);
             end
             else begin
-               set_local_var(5, 2);
+               set_local_var(LVAR_Fridge, 2);
                obj_unlock(Fridge_ptr);
                obj_open(Fridge_ptr);
                use_obj(Fridge_ptr);
             end
          end
-         else if (local_var(5) == 2) then begin
+         else if (local_var(LVAR_Fridge) == 2) then begin
             moving := 1;
             if (tile_distance_objs(self_obj, dude_obj) < 5) then begin
                call reward;
-               set_local_var(5, 3);
+               set_local_var(LVAR_Fridge, 3);
                use_obj(Fridge_ptr);
                obj_close(Fridge_ptr);
                obj_lock(Fridge_ptr);
             end
          end
-         else if (local_var(5) == 3) then begin
+         else if (local_var(LVAR_Fridge) == 3) then begin
             moving := 1;
             target_hex := local_var(7);
             if (my_hex != target_hex) then begin
@@ -155,46 +158,11 @@ procedure talk_p_proc begin
       set_local_var(8, 1);
       float_msg(self_obj, mstr(201), FLOAT_MSG_NORMAL);
       display_msg(mstr(202));
-      stuff := create_object(PID_SMALL_ENERGY_CELL, 0, 0);
-      add_obj_to_inven(dude_obj, stuff);
-      stuff := create_object(PID_MICRO_FUSION_CELL, 0, 0);
-      add_obj_to_inven(dude_obj, stuff);
-      stuff := create_object(PID_BUFFOUT, 0, 0);
-      add_obj_to_inven(dude_obj, stuff);
-      stuff := create_object(PID_MENTATS, 0, 0);
-      add_obj_to_inven(dude_obj, stuff);
-      stuff := create_object(PID_STIMPAK, 0, 0);
-      add_obj_to_inven(dude_obj, stuff);
-      stuff := create_object(PID_STIMPAK, 0, 0);
-      add_obj_to_inven(dude_obj, stuff);
-      stuff := create_object(PID_FRAG_GRENADE, 0, 0);
-      add_obj_to_inven(dude_obj, stuff);
-      stuff := create_object(PID_FRAG_GRENADE, 0, 0);
-      add_obj_to_inven(dude_obj, stuff);
-      stuff := create_object(PID_PLASMA_GRENADE, 0, 0);
-      add_obj_to_inven(dude_obj, stuff);
-      stuff := create_object(PID_PLASMA_GRENADE, 0, 0);
-      add_obj_to_inven(dude_obj, stuff);
-      stuff := create_object(PID_PULSE_GRENADE, 0, 0);
-      add_obj_to_inven(dude_obj, stuff);
-      stuff := create_object(PID_PULSE_GRENADE, 0, 0);
-      add_obj_to_inven(dude_obj, stuff);
-      stuff := create_object(PID_5MM_JHP, 0, 0);
-      add_obj_to_inven(dude_obj, stuff);
-      stuff := create_object(PID_5MM_JHP, 0, 0);
-      add_obj_to_inven(dude_obj, stuff);
-      stuff := create_object(PID_5MM_JHP, 0, 0);
-      add_obj_to_inven(dude_obj, stuff);
-      stuff := create_object(PID_5MM_JHP, 0, 0);
-      add_obj_to_inven(dude_obj, stuff);
-      stuff := create_object(PID_5MM_AP, 0, 0);
-      add_obj_to_inven(dude_obj, stuff);
-      stuff := create_object(PID_5MM_AP, 0, 0);
-      add_obj_to_inven(dude_obj, stuff);
-      stuff := create_object(PID_5MM_AP, 0, 0);
-      add_obj_to_inven(dude_obj, stuff);
-      stuff := create_object(PID_5MM_AP, 0, 0);
-      add_obj_to_inven(dude_obj, stuff);
+      call give_item(dude_obj, {
+         PID_SMALL_ENERGY_CELL: 1, PID_MICRO_FUSION_CELL: 1,
+         PID_BUFFOUT: 1, PID_MENTATS: 1, PID_STIMPAK: 2,
+         PID_FRAG_GRENADE: 2, PID_PLASMA_GRENADE: 2, PID_PULSE_GRENADE: 2,
+         PID_5MM_JHP: 4, PID_5MM_AP: 4});
    end
    else begin
       if (moving) then begin
@@ -203,11 +171,11 @@ procedure talk_p_proc begin
       else begin
          if day then begin
             get_reaction
-            if (local_var(4)) then begin
+            if (local_var(LVAR_Herebefore)) then begin
                call garret02a;
             end
             else begin
-               set_local_var(4, 1);
+               set_local_var(LVAR_Herebefore, 1);
                if (local_var(LVAR_reaction_level) >= NEUTRAL) then begin
                   call garret04;
                end
@@ -287,17 +255,17 @@ procedure reward begin
    CASH := 0;
    robbed := 0;
    display_msg(mstr(203));
+
    if good_critter_reaction then begin
       call goodstuff;
    end
-   else begin
-      if (local_var(LVAR_reaction_level) >= NEUTRAL) then begin
-         call neutstuff;
-      end
-      else begin
-         call badstuff;
-      end
+   else if (local_var(LVAR_reaction_level) >= NEUTRAL) then begin
+      call neutstuff;
    end
+   else begin
+      call badstuff;
+   end
+
    if (flare_count) then begin
       if (flare_count > 1) then begin
          display_msg(mstr(206));
@@ -325,7 +293,7 @@ end
 
 procedure done begin
    moving := 0;
-   set_local_var(5, 4);
+   set_local_var(LVAR_Fridge, 4);
 end
 
 procedure garret00a begin

Fallout2/Fallout1in2/Mapper/source/scripts/07bos/GENINIT.ssl
@@ -32,7 +32,7 @@ procedure start begin
    if Only_Once then begin
       Only_Once := 0;
       set_self_team(TEAM_BROTHERHOOD);
-      if horrible_critter_reaction then begin
+      if no_critter_reaction then begin
          temp := random(BAD, NEUTRAL);
          set_local_var(LVAR_reaction_level, temp);
       end

Fallout2/Fallout1in2/Mapper/source/scripts/GlobalScripts/gl_fo1mechanics.ssl
@@ -104,6 +104,9 @@ procedure general begin
    // Change the credits and wordlmap music tracks to Fo1 files.
    VOODOO_fo1_music;
 
+   // Change talking head reaction value conditions to that of Fo1.
+   VOODOO_talking_head_mood;
+
    // Enable / Disable looting Geckos based on perk.
    call gecko_skinning;
 end

Fallout2/Fallout1in2/Mapper/source/scripts/MAIN/IAN.ssl
@@ -232,8 +232,7 @@ end
 procedure talk_p_proc begin
    set_tma_data(NAME, 1000, -1, -1, 1109, 1109);
 
-   if Ian_In_Party then
-      self_look_at_dude;
+   if Ian_In_Party then self_look_at_dude;
    dude_look_at_self;
    get_reaction
 

Fallout2/Fallout1in2/Mapper/source/scripts/headers/modreact.h
@@ -1,5 +1,26 @@
 #pragma once
 
+/*
+
+   NOTES:
+      We are using VOODOO_talking_head_mood in gl_fo1mechanics.ssl to overwrite the Fo2 talking head animated reaction conditions with the ones from Fo1.
+
+      Fo1:
+         if (reaction <= 25) return 0; // result in bad_fidget
+         if (reaction > 75 ) return 2; // result in good_fidget
+         return 1; // result in neutral_fidget
+
+      Fo2:
+         if (reaction <= -75) return 0;
+         if (reaction <= -50) return 0;
+         if (reaction <= -25) return 0;
+         if (reaction <= -10) return 0; // don't ask me why there are more checks
+         if (reaction > 10)   return 2;
+         return 1;
+
+*/
+
+
 #define TOWN_REPUTATION                 global_var(TOWN_REP_VAR)
 
 /* This set of procedures will find out what the reaction level is. Additionally,
@@ -11,20 +32,19 @@ It will also write in how to modify the reaction level by a level. */
 #define NEUTRAL                         (2)
 #define GOOD                            (3)
 
-// Fo1 was 0 to 100, in Fo2 it is -100 to 100, so we are trying to adapt
-#define LOW_REACTION                    (-50)   // Fo1: (25)
-#define BASE_REACTION                   (0)     // Fo1: (50)
-#define HIGH_REACTION                   (50)    // Fo1: (75)
+#define LOW_REACTION                    (25)
+#define BASE_REACTION                   (50)
+#define HIGH_REACTION                   (75)
 
-#define MIN_REACTION                    (-100)  // Fo1: (0)
-#define MAX_REACTION                    (100)   // Fo1: (100)
+#define MIN_REACTION                    (0)
+#define MAX_REACTION                    (100)
 
-#define NORMAL_BOOST                    (20)    // Fo1: (10)
-#define HUGE_BOOST                      (50)    // Fo1: (25)
+#define NORMAL_BOOST                    (10)
+#define HUGE_BOOST                      (25)
 
-#define REACTION_BONUS_BERSERKER        (40)    // Fo1: (20)
-#define REACTION_BONUS_CHAMPION         (40)    // Fo1: (20)
-#define REACTION_BONUS_CHILDKILLER      (60)    // Fo1: (30)
+#define REACTION_BONUS_BERSERKER        (20)
+#define REACTION_BONUS_CHAMPION         (20)
+#define REACTION_BONUS_CHILDKILLER      (30)
 
 variable Static_Reaction:=0;            // This adds in Reputations, Perks, Karma, and such
 variable Evil_Critter:=0;               // 0 == Good Critter, 1 == Bad Critter
@@ -44,6 +64,7 @@ variable reaction_bonus_karma:=0;
 
 // Checks the reaction_levels from a critter
 #define reaction_level                  local_var(LVAR_reaction_level)
+#define no_critter_reaction             (local_var(LVAR_reaction_level) == NO_REACTION)
 #define horrible_critter_reaction       (local_var(LVAR_reaction_level) <= NO_REACTION)
 #define bad_critter_reaction            (local_var(LVAR_reaction_level) <= BAD)
 #define neutral_critter_reaction        (local_var(LVAR_reaction_level) == NEUTRAL)
@@ -54,25 +75,24 @@ variable reaction_bonus_karma:=0;
                            set_local_var(LVAR_reaction, BASE_REACTION);    \
                            set_local_var(LVAR_reaction_level, NEUTRAL);    \
                            set_local_var(LVAR_got_reaction, 1);            \
-                                                  /* We double the reaction check, because -100 to 100 instead of 0 to 100! */      \
-                           set_local_var(LVAR_reaction, local_var(LVAR_reaction) + (2 * ((5 * dude_charisma) - 25)));               \
-                           set_local_var(LVAR_reaction, local_var(LVAR_reaction) + (2 * (10 * dude_perk(PERK_presence))));          \
+                           set_local_var(LVAR_reaction, local_var(LVAR_reaction) + ((5 * dude_charisma) - 25));                     \
+                           set_local_var(LVAR_reaction, local_var(LVAR_reaction) + (10 * dude_perk(PERK_presence)));                \
                            if dude_perk(PERK_karma_beacon_perk) then begin /* Added support for Fo2 perk */                         \
                               set_local_var(LVAR_reaction, local_var(LVAR_reaction) * 2);                                           \
                            end                                                                                                      \
                            if dude_perk(PERK_cult_of_personality) then begin                                                        \
                               if (global_var(GVAR_PLAYER_REPUTATION) > 0) then begin                                                \
-                                 set_local_var(LVAR_reaction, local_var(LVAR_reaction) + (2 * global_var(GVAR_PLAYER_REPUTATION))); \
+                                 set_local_var(LVAR_reaction, local_var(LVAR_reaction) + global_var(GVAR_PLAYER_REPUTATION));       \
                               end                                                                                                   \
                               else begin                                                                                            \
-                                 set_local_var(LVAR_reaction, local_var(LVAR_reaction) - (2 * global_var(GVAR_PLAYER_REPUTATION))); \
+                                 set_local_var(LVAR_reaction, local_var(LVAR_reaction) - global_var(GVAR_PLAYER_REPUTATION));       \
                               end                                                                                                   \
                            end                                                                                                      \
                            else if (local_var(LVAR_base_reaction) == 1) then begin                                                  \
-                              set_local_var(LVAR_reaction, local_var(LVAR_reaction) - (2 * global_var(GVAR_PLAYER_REPUTATION)));    \
+                              set_local_var(LVAR_reaction, local_var(LVAR_reaction) - global_var(GVAR_PLAYER_REPUTATION));          \
                            end                                                                                                      \
                            else begin                                                                                               \
-                              set_local_var(LVAR_reaction, local_var(LVAR_reaction) + (2 * global_var(GVAR_PLAYER_REPUTATION)));    \
+                              set_local_var(LVAR_reaction, local_var(LVAR_reaction) + global_var(GVAR_PLAYER_REPUTATION));          \
                            end                                                                                                      \
                            if has_rep_childkiller then begin                                                         \
                               set_local_var(LVAR_reaction, local_var(LVAR_reaction) - REACTION_BONUS_CHILDKILLER);   \
@@ -99,17 +119,14 @@ variable reaction_bonus_karma:=0;
 
 
 #define LevelToReact    if (local_var(LVAR_reaction_level) == BAD) then begin       \
-                           /*set_local_var(LVAR_reaction, random(1, 25));*/         \
-                           set_local_var(LVAR_reaction, random(-99, -50));          \
+                           set_local_var(LVAR_reaction, random(1, 25));             \
                         end                                                         \
                         else                                                        \
                         if (local_var(LVAR_reaction_level) == NEUTRAL) then begin   \
-                           /*set_local_var(LVAR_reaction, random(26, 75));*/        \
-                           set_local_var(LVAR_reaction, random(-49, 50));           \
+                           set_local_var(LVAR_reaction, random(26, 75));            \
                         end                                                         \
                         else begin                                                  \
-                           /*set_local_var(LVAR_reaction, random(76, 100));*/       \
-                           set_local_var(LVAR_reaction, random(51, 100));           \
+                           set_local_var(LVAR_reaction, random(76, 100));           \
                         end
 
 

Fallout2/Fallout1in2/Mapper/source/scripts/headers/voodoo.h
@@ -37,13 +37,18 @@ variable $addr;
 // This will change the credits and wordlmap music tracks, so that
 // the HQ music pack will not overwrite the Fo1 music with Fo2 tracks.
 #define VOODOO_fo1_music \
-               write_string(0x501A40, "maybe");    \
+               write_string(0x501A40, "maybe"); \
                write_string(0x50F790, "03WORLD")
 
 // This will change the resting strings in the PIPBOY.MSG to start at 320.
 // This is used in combination with VOODOO_rest_till_0600 to change the resting times.
 #define VOODOO_rest_strings \
                write_int(0x499746, 320)
+               
+// This changes the animated head reaction values. See modreact.h for more details.
+#define VOODOO_talking_head_mood \
+               write_byte(0x4A29F5, 25); \
+               write_byte(0x4A2A0D, 75)
 
 /////////////////////////////////////////////////// AUTOMAGICK ZONE ///////////////////////////////////////////////////
 //