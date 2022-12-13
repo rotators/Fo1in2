bdd7530caf187daf39f0bc15d2ea144cb93e5b8a
Lexx2k <lexxnet@web.de>
2022-12-13 12:03:49 +0100

    Mariposa robot refactoring.

--- Fallout2/Fallout1in2/Mapper/source/scripts/headers/area/mbase.h
+++ Fallout2/Fallout1in2/Mapper/source/scripts/headers/area/mbase.h
@@ -59,6 +59,40 @@ variable ff_emitter;
                                     destroy_object(self_obj);                                                          \
                                  end
 
+// Robobrain settings
+#define robo_full_sensors     0
+#define robo_min_sensors      1
+#define set_robo_full_sensors       set_global_var(GVAR_ROBCTRL_SENSORS_HOWMUCH, robo_full_sensors)
+#define set_robo_minimal_sensors    set_global_var(GVAR_ROBCTRL_SENSORS_HOWMUCH, robo_min_sensors)
+#define get_robo_full_sensors       (global_var(GVAR_ROBCTRL_SENSORS_HOWMUCH) == robo_full_sensors)
+#define get_robo_minimal_sensors    (global_var(GVAR_ROBCTRL_SENSORS_HOWMUCH) == robo_min_sensors)
+
+#define robo_max_movement     0
+#define robo_half_movement    1
+#define robo_min_movement     2
+#define set_robo_max_movement       set_global_var(GVAR_ROBCTRL_MOVEMENT_HOWMUCH, robo_max_movement)
+#define set_robo_half_movement      set_global_var(GVAR_ROBCTRL_MOVEMENT_HOWMUCH, robo_half_movement)
+#define set_robo_min_movement       set_global_var(GVAR_ROBCTRL_MOVEMENT_HOWMUCH, robo_min_movement)
+#define get_robo_max_movement       (global_var(GVAR_ROBCTRL_MOVEMENT_HOWMUCH) == robo_max_movement)
+#define get_robo_half_movement      (global_var(GVAR_ROBCTRL_MOVEMENT_HOWMUCH) == robo_half_movement)
+#define get_robo_min_movement       (global_var(GVAR_ROBCTRL_MOVEMENT_HOWMUCH) == robo_min_movement)
+
+#define robo_attack_unauth_pests 0
+#define robo_attack_all_pests    1
+#define robo_attack_large_pests  2
+#define robo_attack_small_pests  3
+#define robo_attack_no_pests     4
+#define set_robo_attack_unauth_pests   set_global_var(GVAR_ROBCTRL_PESTS_HOWMUCH, robo_attack_unauth_pests)
+#define set_robo_attack_all_pests      set_global_var(GVAR_ROBCTRL_PESTS_HOWMUCH, robo_attack_all_pests)
+#define set_robo_attack_large_pests    set_global_var(GVAR_ROBCTRL_PESTS_HOWMUCH, robo_attack_large_pests)
+#define set_robo_attack_small_pests    set_global_var(GVAR_ROBCTRL_PESTS_HOWMUCH, robo_attack_small_pests)
+#define set_robo_attack_no_pests       set_global_var(GVAR_ROBCTRL_PESTS_HOWMUCH, robo_attack_no_pests)
+#define get_robo_attack_unauth_pests   (global_var(GVAR_ROBCTRL_PESTS_HOWMUCH) == robo_attack_unauth_pests)
+#define get_robo_attack_all_pests      (global_var(GVAR_ROBCTRL_PESTS_HOWMUCH) == robo_attack_all_pests)
+#define get_robo_attack_large_pests    (global_var(GVAR_ROBCTRL_PESTS_HOWMUCH) == robo_attack_large_pests)
+#define get_robo_attack_small_pests    (global_var(GVAR_ROBCTRL_PESTS_HOWMUCH) == robo_attack_small_pests)
+#define get_robo_attack_no_pests       (global_var(GVAR_ROBCTRL_PESTS_HOWMUCH) == robo_attack_no_pests)
+
 // Mutant combat alert check
 #define alert_check \
    if (fixed_param == COMBAT_SUBTYPE_TURN) then begin \
