a6bb4f2f834adefaf84c5356e8f1d3e256d03bd2
Lexx2k <lexxnet@web.de>
2023-04-29 16:49:39 +0200

    Moved VOODOO_fo1_music into Sfall.

--- Fallout2/Fallout1in2/Mapper/source/scripts/headers/voodoo.h
+++ Fallout2/Fallout1in2/Mapper/source/scripts/headers/voodoo.h
@@ -45,9 +45,11 @@ variable $addr;
 
 // This will change the credits and wordlmap music tracks, so that
 // the HQ music pack will not overwrite the Fo1 music with Fo2 tracks.
+/*
 #define VOODOO_fo1_music \
                write_string(0x501A40, "maybe"); \
                write_string(0x50F790, "03WORLD")
+*/
 
 // This will change the resting strings in the PIPBOY.MSG to start at 320.
 // This is used in combination with VOODOO_rest_till_0600 to change the resting times.
