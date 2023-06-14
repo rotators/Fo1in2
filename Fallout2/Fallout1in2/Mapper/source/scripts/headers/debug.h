cd4a55123d33fb34630d8cd278be7e651c1066ac
Lexx2k <lexxnet@web.de>
2023-06-14 17:58:15 +0200

    Initial debug message overhaul

--- Fallout2/Fallout1in2/Mapper/source/scripts/headers/debug.h
+++ Fallout2/Fallout1in2/Mapper/source/scripts/headers/debug.h
@@ -2,5 +2,44 @@
 
 #include "global.h"
 
-#define debug_mode  (global_var(GVAR_ENABLE_DEBUG_MESSAGES) == 1)
-#define debug(x)    if debug_mode then display_msg(x)
+#define debug_mode   (global_var(GVAR_ENABLE_DEBUG_MESSAGES) == 1)
+//#define debug(x)     if debug_mode then display_msg(x)
+
+/*
+   To enable styles and colors in the debug console, run a .bat file with:
+      reg add HKEY_CURRENT_USER\Console /v VirtualTerminalLevel /t REG_DWORD /d 0x00000001 /f
+*/
+#define STYLE_RESET           "[0m"
+#define STYLE_BOLD            "[1m"
+#define STYLE_UNDERLINE       "[4m"
+#define STYLE_INVERSE         "[7m"
+
+#define COLOR_BLACK(x)        "[30m" + x + STYLE_RESET
+#define COLOR_RED(x)          "[31m" + x + STYLE_RESET
+#define COLOR_GREEN(x)        "[32m" + x + STYLE_RESET
+#define COLOR_YELLOW(x)       "[33m" + x + STYLE_RESET
+#define COLOR_BLUE(x)         "[34m" + x + STYLE_RESET
+#define COLOR_MAGENTA(x)      "[35m" + x + STYLE_RESET
+#define COLOR_CYAN(x)         "[36m" + x + STYLE_RESET
+#define COLOR_WHITE(x)        "[37m" + x + STYLE_RESET
+
+#define COLOR_BG_BLACK        "[100m"
+#define COLOR_BG_RED          "[101m"
+#define COLOR_BG_GREEN        "[102m"
+#define COLOR_BG_YELLOW       "[103m"
+#define COLOR_BG_BLUE         "[104m"
+#define COLOR_BG_MAGENTA      "[105m"
+#define COLOR_BG_CYAN         "[106m"
+#define COLOR_BG_WHITE        "[107m"
+
+// Default is green text
+#define debug(x)           debug_msg(" " + STYLE_BOLD + COLOR_GREEN(x))
+
+#define debug_black(x)     debug_msg(" " + COLOR_BLACK(x))
+#define debug_red(x)       debug_msg(" " + COLOR_RED(x))
+#define debug_green(x)     debug_msg(" " + COLOR_GREEN(x))
+#define debug_yellow(x)    debug_msg(" " + COLOR_YELLOW(x))
+#define debug_blue(x)      debug_msg(" " + COLOR_BLUE(x))
+#define debug_magenta(x)   debug_msg(" " + COLOR_MAGENTA(x))
+#define debug_cyan(x)      debug_msg(" " + COLOR_CYAN(x))
+#define debug_white(x)     debug_msg(" " + COLOR_WHITE(x))
