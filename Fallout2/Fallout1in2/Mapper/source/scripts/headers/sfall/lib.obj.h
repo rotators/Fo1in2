84257d56fc4a053a30bdc7d033c51a849e78ed07
NovaRain <novarain@gmail.com>
2023-11-24 22:06:38 +0800

    - Fixed: Footsteps mod would crash the game when triggering corpse aging on map enter.
    (#195, #201)
    
    Updated sfall headers.

---
+++ Fallout2/Fallout1in2/Mapper/source/scripts/headers/sfall/lib.obj.h
@@ -0,0 +1,12 @@
+#ifndef LIB_OBJ_H
+#define LIB_OBJ_H
+
+#include "define_lite.h"
+#include "define_extra.h"
+
+#define obj_name_safe(obj)          (obj_name(obj) if obj else "(null)")
+#define get_active_weapon(critter)  critter_inven_obj(critter, (2 - active_hand) if critter == dude_obj else INVEN_TYPE_RIGHT_HAND)
+#define obj_has_lof_to_obj(critter, target)           (obj_blocking_line(critter, tile_num(target), BLOCKING_TYPE_SHOOT) == target)
+#define obj_can_hear_and_shoot_obj(critter, target)   (obj_can_hear_obj(critter, target) and obj_has_lof_to_obj(critter, target))
+
+#endif
