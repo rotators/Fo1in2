4da54f0bbc9b9c5b655ab7a5b7b6dab033efd06f
Lexx2k <lexxnet@web.de>
2024-05-22 14:16:00 +0200

    - Fixed: Mr. Handy and Floating Eye companions use base game repair mechanics for healing now.

--- Fallout2/Fallout1in2/Mapper/source/scripts/headers/maps/lablades.h
+++ Fallout2/Fallout1in2/Mapper/source/scripts/headers/maps/lablades.h
@@ -18,7 +18,7 @@
 //==================================================================
 
 // All Map Vars need to start w/ MVAR_
-#define MVAR_Already_Give_Mike_Caps  (0)
+#define MVAR_Already_Give_Mike_Caps  (0) // Unused
 #define MVAR_InvasionForcesCreated   (1) // Have the invasion forces been created?
 #define MVAR_Car_Kids_created        (2) // Have the car kids spawned before?
 
