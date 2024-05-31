019ca8bc3306937a824105d40c9436f7a52bfe2f
Lexx2k <lexxnet@web.de>
2024-05-31 14:08:29 +0200

    - Updated to experimental Sfall - Deprecated huge amount of the wm script. Keeping it in files for now because nostalgia.

--- Fallout2/Fallout1in2/Mapper/source/scripts/headers/gl_worldmap.h
+++ Fallout2/Fallout1in2/Mapper/source/scripts/headers/gl_worldmap.h
@@ -6,7 +6,17 @@
 #define WORLDMAP_TERRAIN_MOUNTAIN (3)
 #define WORLDMAP_TERRAIN_OCEAN    (4)
 
+/*
 #define is_terrain_desert   (global_var(GVAR_WORLDMAP_TERRAIN) == WORLDMAP_TERRAIN_DESERT)
 #define is_terrain_city     (global_var(GVAR_WORLDMAP_TERRAIN) == WORLDMAP_TERRAIN_CITY)
 #define is_terrain_mountain (global_var(GVAR_WORLDMAP_TERRAIN) == WORLDMAP_TERRAIN_MOUNTAIN)
 #define is_terrain_ocean    (global_var(GVAR_WORLDMAP_TERRAIN) == WORLDMAP_TERRAIN_OCEAN)
+*/
+
+#define mstr_unknown       mstr_worldmap(1004)
+
+#define is_terrain_desert           (get_current_terrain_name == mstr_worldmap(1000) or get_current_terrain_name == mstr_worldmap(1006))
+#define is_terrain_mountain         (get_current_terrain_name == mstr_worldmap(1001) or get_current_terrain_name == mstr_worldmap(1005))
+#define is_terrain_city             (get_current_terrain_name == mstr_worldmap(1002))
+#define is_terrain_ocean            (get_current_terrain_name == mstr_worldmap(1003))
+           
