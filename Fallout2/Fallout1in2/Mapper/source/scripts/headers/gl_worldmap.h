9b88cfc20737ea5fb603c88b45bcb17973ecaf45
Lexx2k <lexxnet@web.de>
2024-05-31 14:38:01 +0200

    - Enabled auto-caps counter by default.

--- Fallout2/Fallout1in2/Mapper/source/scripts/headers/gl_worldmap.h
+++ Fallout2/Fallout1in2/Mapper/source/scripts/headers/gl_worldmap.h
@@ -1,12 +1,12 @@
 #pragma once
 
+/*
 #define WORLDMAP_TERRAIN_UNKNOWN  (0)
 #define WORLDMAP_TERRAIN_CITY     (1)
 #define WORLDMAP_TERRAIN_DESERT   (2)
 #define WORLDMAP_TERRAIN_MOUNTAIN (3)
 #define WORLDMAP_TERRAIN_OCEAN    (4)
 
-/*
 #define is_terrain_desert   (global_var(GVAR_WORLDMAP_TERRAIN) == WORLDMAP_TERRAIN_DESERT)
 #define is_terrain_city     (global_var(GVAR_WORLDMAP_TERRAIN) == WORLDMAP_TERRAIN_CITY)
 #define is_terrain_mountain (global_var(GVAR_WORLDMAP_TERRAIN) == WORLDMAP_TERRAIN_MOUNTAIN)
