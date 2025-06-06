041f117964b9b4ee74e75f32b5ea99043e73196d
lexx2k <lexxnet@web.de>
2025-06-06 21:44:14 +0200

    Necropolis - Fixed: If the player fixed the Necropolis water pump, the good ending would play, even if the town had already died of dehydration before.

--- Fallout2/Fallout1in2/Mapper/source/scripts/headers/global.h
+++ Fallout2/Fallout1in2/Mapper/source/scripts/headers/global.h
@@ -4039,8 +4039,8 @@
 #define RESERVED_FOR_FALLOUT_FIXT               (4897)
 #define RESERVED_FOR_FALLOUT_FIXT               (4898)
 #define RESERVED_FOR_FALLOUT_FIXT               (4899)
-#define RESERVED_FOR_FALLOUT_FIXT               (4900)
-#define RESERVED_FOR_FALLOUT_FIXT               (4901)*/
+#define RESERVED_FOR_FALLOUT_FIXT               (4900)*/
+#define GVAR_NECROPOLIS_PARCHED                 (4901)
 #define GVAR_ENABLE_PARTY_LEVELING              (4902)
 #define GVAR_TALKED_TO_VREE                     (4903)
 #define GVAR_ENABLE_FIXT_CHESS                  (4904)
