135d0eb578cae3407396744b9b6992b96a00297b
Lexx2k <lexxnet@web.de>
2026-08-07 08:15:24 +0200

    More CE related changes

--- Fallout2/Fallout1in2/Mapper/source/scripts/headers/ce/ce.h
+++ Fallout2/Fallout1in2/Mapper/source/scripts/headers/ce/ce.h
@@ -2,7 +2,13 @@
 #define CE_H
 
 // CE-only metarules
-#define set_npc_reaction_thresholds(neutral, good)                 sfall_func2("set_npc_reaction_thresholds", neutral, good)
 #define set_party_member_cc_msg_ids(pid, start_msg_id, end_msg_id) sfall_func3("set_party_member_cc_msg_ids", pid, start_msg_id, end_msg_id)
+#define rest_option_msgs(base_msg_id) sfall_func1("rest_option_msgs", base_msg_id)
+#define set_rest_option(rest_option, value) sfall_func2("set_rest_option", rest_option, value)
+
+#define REST_OPTION_MORNING 8
+#define REST_OPTION_NOON 9
+#define REST_OPTION_EVENING 10
+#define REST_OPTION_MIDNIGHT 11
 
 #endif
