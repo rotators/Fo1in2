3a293600a0c52a82536887711a1e089e6cf95ed5
Lexx2k <lexxnet@web.de>
2026-08-08 21:21:58 +0200

    Cleanup

--- Fallout2/Fallout1in2/Mapper/source/scripts/headers/gl_worldmap.h
+++ Fallout2/Fallout1in2/Mapper/source/scripts/headers/gl_worldmap.h
@@ -1,18 +1,5 @@
 #pragma once
 
-/*
-#define WORLDMAP_TERRAIN_UNKNOWN  (0)
-#define WORLDMAP_TERRAIN_CITY     (1)
-#define WORLDMAP_TERRAIN_DESERT   (2)
-#define WORLDMAP_TERRAIN_MOUNTAIN (3)
-#define WORLDMAP_TERRAIN_OCEAN    (4)
-
-#define is_terrain_desert   (global_var(GVAR_WORLDMAP_TERRAIN) == WORLDMAP_TERRAIN_DESERT)
-#define is_terrain_city     (global_var(GVAR_WORLDMAP_TERRAIN) == WORLDMAP_TERRAIN_CITY)
-#define is_terrain_mountain (global_var(GVAR_WORLDMAP_TERRAIN) == WORLDMAP_TERRAIN_MOUNTAIN)
-#define is_terrain_ocean    (global_var(GVAR_WORLDMAP_TERRAIN) == WORLDMAP_TERRAIN_OCEAN)
-*/
-
 #define mstr_unknown       mstr_worldmap(1004)
 
 #define is_terrain_desert           (get_current_terrain_name == mstr_worldmap(1000) or get_current_terrain_name == mstr_worldmap(1006))
