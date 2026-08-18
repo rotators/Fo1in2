978615489428fd49323d1b12a101c17401a0155f
Lexx2k <forall@advexx.de>
2026-08-18 08:26:21 +0200

    Updated to latest CE version and cleaned up unused functions.

--- Fallout2/Fallout1in2/Mapper/source/scripts/headers/ce/ce.h
+++ Fallout2/Fallout1in2/Mapper/source/scripts/headers/ce/ce.h
@@ -3,7 +3,6 @@
 
 // CE-only metarules
 #define set_party_member_cc_msg_ids(pid, start_msg_id, end_msg_id) sfall_func3("set_party_member_cc_msg_ids", pid, start_msg_id, end_msg_id)
-#define encounter_intros(toggle) sfall_func1("encounter_intros", toggle)
 #define rest_option_msgs(base_msg_id) sfall_func1("rest_option_msgs", base_msg_id)
 #define set_rest_option(rest_option, value) sfall_func2("set_rest_option", rest_option, value)
 
